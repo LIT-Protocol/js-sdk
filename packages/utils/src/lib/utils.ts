b49fde2f9f1d3c3ae54d117526dc1d8ad9588180
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-08-23 14:55:36 +0100

    test: publish

--- packages/utils/src/lib/utils.ts
+++ packages/utils/src/lib/utils.ts
@@ -1,7 +1,15 @@
+import { LIT_CHAINS } from "@litprotocol-dev/constants";
+
 /**
  * Test 8
  */
 export const utils = () => {
   console.log("[utils] has been called!");
   return 'utils()';
+}
+
+export const testConstants = () => {
+
+  console.log("LIT_CHAINS:", LIT_CHAINS);
+
 }
