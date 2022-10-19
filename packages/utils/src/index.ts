1031c998aa764360c9263518ca9fa3451449d126
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-10-19 17:35:02 +0100

    add .gitattributes

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,8 +4,8 @@ export * as cosmos from './lib/browser/chains/cosmos';
 export * as eth from './lib/browser/chains/eth';
 export * as lit from './lib/browser/lit';
 export * from './lib/browser/humanizer';
-
 export * from './lib/browser/crypto';
+
 /** ------ Access Control Conditions ----- */
 export * from './lib/browser/access_control_conditions/hashing';
 export * from './lib/browser/access_control_conditions/canonicalFormatter';
