6c1cb0c07c4c1e8e9f89a16aa469595f2a4fe904
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-11-15 11:07:54 +0000

    update readme

--- tools/scripts/gen-readme.mjs
+++ tools/scripts/gen-readme.mjs
@@ -9,19 +9,64 @@ import { listDirsRelative } from './utils.mjs';
 const readmePath = join('README.md');
 const readme = readFileSync(readmePath, 'utf8');
 
+const badge = (lib, text) => {
+
+    let color = 'orange';
+
+    if (text === 'universal') {
+        color = '8A6496';
+    }
+    if (text === 'bundled') {
+        color = '17224B';
+    }
+    if (text === 'browser') {
+        color = 'E98869';
+    }
+
+    return `![${lib}](https://img.shields.io/badge/-${text}-${color} "${lib}")`;
+}
+
+const jsdelivLink = (lib) => {
+    return `[![](https://data.jsdelivr.com/v1/package/npm/${lib}-vanilla/badge)](https://www.jsdelivr.com/package/npm/${lib}-vanilla)`;
+}
+
 const libs = (await listDirsRelative('packages', false)).map(lib => lib.replace('packages/', ''));
 
 // create rows to array
-const rows = libs.map(lib => {
+let universals = [];
+let browsers = [];
+let bundled = [];
+
+libs.map(lib => {
     const pkg = JSON.parse(readFileSync(`packages/${lib}/package.json`, 'utf8'));
     const { name, description, version, tags } = pkg;
-    return `| [${name}](packages/${lib}) | ${tags?.join(', ')} | ${version} |`;
+
+    const _package = `[${name}](packages/${lib})`;
+    const _vanillaJs = jsdelivLink(name);
+    const _tag = badge(lib, tags[0]);
+    const _version = version;
+
+    const content = `| ${_package} | ${_vanillaJs}| ${_tag} | ${_version} |`;
+
+    if (tags[0] === 'universal') {
+        universals.push(content);
+    }
+    if (tags[0] === 'browser') {
+        browsers.push(content);
+    }
+    if (tags[0] === 'bundled') {
+        bundled.push(content);
+    }
 });
 
+let rows = [...bundled, ...universals, ...browsers,];
+
 const tables = {
-    "headers": ["Package", "Tags", "Version"],
+    "headers": ["Package", "Vanilla JS", "Category", "Version"],
     "rows": rows
 }
+
+console.log(rows);
 
 // make table to github markdown
 const table = (tables) => {
