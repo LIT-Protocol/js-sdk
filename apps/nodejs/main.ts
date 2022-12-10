3e1c7f50c75c3349ab9a3e5cc13056b717662d11
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-12-10 21:53:20 +0000

    add old tests & sol doesnt work

--- apps/nodejs/main.ts
+++ apps/nodejs/main.ts
@@ -4,8 +4,11 @@ import { hasItems } from './helper';
 
 import * as accessControlConditions from '@lit-protocol/access-control-conditions';
 import * as authBrowser from '@lit-protocol/auth-browser';
+import * as blsSdk from '@lit-protocol/bls-sdk';
+import * as constants from '@lit-protocol/constants';
 import * as contractsSdk from '@lit-protocol/contracts-sdk';
 import * as crypto from '@lit-protocol/crypto';
+import * as ecdsaSdk from '@lit-protocol/ecdsa-sdk';
 import * as encryption from '@lit-protocol/encryption';
 import * as litNodeClient from '@lit-protocol/lit-node-client';
 import * as misc from '@lit-protocol/misc';
@@ -15,8 +18,11 @@ import * as uint8arrays from '@lit-protocol/uint8arrays';
 
 console.log("accessControlConditions:", hasItems(accessControlConditions));
 console.log("authBrowser:", hasItems(authBrowser));
+console.log("blsSdk:", hasItems(blsSdk));
+console.log("constants:", hasItems(constants));
 console.log("contractsSdk:", hasItems(contractsSdk));
 console.log("crypto:", hasItems(crypto));
+console.log("ecdsaSdk:", hasItems(ecdsaSdk));
 console.log("encryption:", hasItems(encryption));
 console.log("litNodeClient:", hasItems(litNodeClient));
 console.log("misc:", hasItems(misc));
