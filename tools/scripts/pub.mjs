0e9a8d8cafce90d1329a8816a3820a6a65eede70
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-12-12 13:14:48 -0500

    exit after publishing

--- tools/scripts/pub.mjs
+++ tools/scripts/pub.mjs
@@ -1,8 +1,16 @@
 // # Usage: node tools/scripts/pub.mjs
 // import { exec } from 'child_process';
 
-import { exit } from "process";
-import { asyncForEach, greenLog, listDirsRecursive, runCommand, getArgs, childRunCommand, spawnCommand } from "./utils.mjs";
+import { exit } from 'process';
+import {
+  asyncForEach,
+  greenLog,
+  listDirsRecursive,
+  runCommand,
+  getArgs,
+  childRunCommand,
+  spawnCommand,
+} from './utils.mjs';
 
 const args = getArgs();
 const FLAG = args[0];
@@ -12,18 +20,20 @@ const FLAG2 = args[2];
 let dirs = await listDirsRecursive('dist/packages', false);
 
 if (FLAG === '--filter') {
-    dirs = dirs.filter((dir) => dir.includes(VALUE));
+  dirs = dirs.filter((dir) => dir.includes(VALUE));
 }
 
 dirs.forEach((dir) => {
-    greenLog(`Publishing ${dir}`);
+  greenLog(`Publishing ${dir}`);
 
-    if (FLAG2 !== '--dry-run') {
-        spawnCommand('npm', ['publish', '--access', 'public'], {
-            cwd: dir
-        });
-        // exec(`cd ${dir} && npm publish --access public`);
-    } else {
-        greenLog(`Dry run, skipping publish`);
-    }
-})
+  if (FLAG2 !== '--dry-run') {
+    spawnCommand('npm', ['publish', '--access', 'public'], {
+      cwd: dir,
+    });
+    // exec(`cd ${dir} && npm publish --access public`);
+  } else {
+    greenLog(`Dry run, skipping publish`);
+  }
+});
+
+exit(0);
