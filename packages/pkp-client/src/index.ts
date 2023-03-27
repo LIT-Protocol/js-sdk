8ff84b1c1bfc789d5341f615e2aba12f7f8acba6
sarahzdev <sarah.hg.zhang@gmail.com>
2023-03-27 10:56:57 -0700

    fix: update export

--- packages/pkp-client/src/index.ts
+++ packages/pkp-client/src/index.ts
@@ -1,5 +1,8 @@
 // ----- autogen:polyfills:start  -----
 //
 // ----- autogen:polyfills:end  -----
+import { Auth as LitAuth } from './lib/auth';
 
 export * from './lib/pkp-client';
+
+export default LitAuth;
