90fa999920f1e47fca4aec4bd7f3522d8eb565a0
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-04-04 00:07:19 -0400

    juno works nicely

--- packages/constants/src/lib/constants/constants.ts
+++ packages/constants/src/lib/constants/constants.ts
@@ -431,6 +431,15 @@ export const LIT_COSMOS_CHAINS: LITChain<LITCosmosChain> = {
     blockExplorerUrls: ['https://explorer.cheqd.io'],
     vmType: 'CVM',
   },
+  juno: {
+    name: 'Juno',
+    symbol: 'JUNO',
+    decimals: 6,
+    chainId: 'juno-1',
+    rpcUrls: ['https://rest.cosmos.directory/juno'],
+    blockExplorerUrls: ['https://www.mintscan.io/juno'],
+    vmType: 'CVM',
+  },
 };
 
 /**
