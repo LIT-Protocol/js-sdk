0765b5522fd129339dfa87e0b889f6ccdd5161f6
Josh Long <josh@litprotocol.com>
2023-05-08 18:15:46 -0400

    add demo

---
+++ apps/demo-lit-auth-client-react/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useState } from 'react';
+import LitLogo from './LitLogo';
+import {Otp} from './Otp';
+
+function App() {
+
+  // ----- autogen:app-name:start  -----
+const [appName, setAppName] = useState('SMS / Email authentication and Signup');
+// ----- autogen:app-name:end  -----
+
+  const [npmRepo, setNpmRepo] = useState('');
+  const [demoRepo, setDemoRepo] = useState('');
+  const [lang, setLang] = useState('json');
+  const [data, setData] = useState({
+    data: {
+      name: 'Lit Protocol',
+      description: 'Threadshold cryptography for the win!',
+    }
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LitLogo />
+        <h4>
+          React Demo for: {appName}<br/>
+          <span>
+            <a target="_blank" href={npmRepo}>npm repo</a>&nbsp;|&nbsp;
+            <a target="_blank" href={demoRepo}>demo repo</a>
+          </span>
+        </h4>
+      </header>
+      <div className="Otp">
+        <Otp/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
