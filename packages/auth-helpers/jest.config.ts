e2b32033f3563425b5adeffd1bebc5ff9bd53da6
Howard Tam <howard.tam.95@gmail.com>
2023-05-11 20:11:24 -0700

    Address PR feedback

--- packages/auth-helpers/jest.config.ts
+++ packages/auth-helpers/jest.config.ts
@@ -12,4 +12,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/auth-helpers',
+  setupFilesAfterEnv: ['../../jest.setup.js'],
 };
