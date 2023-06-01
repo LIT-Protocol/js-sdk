5a499f146dfab1171a3cb57249469de0fb3033f1
Sarah <sarah@gathercontroller.litgateway.com>
2023-06-01 11:52:31 +0000

    add excludes in tsconfig.spec.json

--- tools/scripts/reset.sh
+++ tools/scripts/reset.sh
@@ -1,16 +1,10 @@
 #!/bin/zsh
 
-paths_to_delete=(
-  "./node_modules"
-  "./dist"
-  "./doc"
-  "./tmp"
-  "./yarn-error.log"
-  "./yarn.lock"
-  "./package-lock.json"
-  "./lerna-debug.log"
-)
-
-for path in "${paths_to_delete[@]}"; do
-  rm -rf $path
-done
+rm -rf ./node_modules
+rm -rf ./dist
+rm -rf ./doc
+rm -rf ./tmp
+rm -rf ./yarn-error.log
+rm -rf ./yarn.lock
+rm -rf ./package-lock.json
+rm -rf ./lerna-debug.log
