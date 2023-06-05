9e74f6214e8dae522d4aad042963a9efb29844c8
Josh Long <josh@litprotocol.com>
2023-06-05 08:51:38 -0400

    remove user agent

--- packages/lit-auth-client/src/lib/providers/GoogleProvider.ts
+++ packages/lit-auth-client/src/lib/providers/GoogleProvider.ts
@@ -134,8 +134,6 @@ export default class GoogleProvider extends BaseProvider {
         method: 'GET',
         headers: {
           'content-type': 'application/x-www-form-urlencoded',
-          'user-agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
         },
       }
     );
