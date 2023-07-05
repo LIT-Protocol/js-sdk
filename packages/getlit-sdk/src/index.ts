6d9ba101f1e097b218aee45da66cdfdaec4f300b
Ansonhkg <ansonox@gmail.com>
2023-07-05 18:52:30 +0100

    WIP: pair-programming session #1

--- packages/getlit-sdk/src/index.ts
+++ packages/getlit-sdk/src/index.ts
@@ -1,4 +1,12 @@
 export * from './lib/getlit-sdk';
 import * as LitNodeClient from '@lit-protocol/lit-node-client';
+import { LitNodeClientConfig, LitRelayConfig, OtpProviderOptions } from '@lit-protocol/types';
+import { ethers } from 'ethers';
 
 export { LitNodeClient };
+
+(async () => {
+
+window.Lit = {};
+
+})();
