7eb3b23c9b01367bf56d9d36b61e4ba84bf3d18f
Howard Tam <howard.tam.95@gmail.com>
2023-07-06 17:14:42 -0700

    Fix ci

--- packages/auth-browser/jest.config.ts
+++ packages/auth-browser/jest.config.ts
@@ -1,10 +1,14 @@
 /* eslint-disable */
 export default {
   displayName: 'auth-browser',
-  preset: '../../jest.preset.js',
-
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   transform: {
-    '^.+\\.[tj]s$': ['babel-jest', { cwd: __dirname }],
+    '^.+\\.[tj]s$': 'ts-jest',
   },
   transformIgnorePatterns: ['node_modules/(?!(@walletconnect)/)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
