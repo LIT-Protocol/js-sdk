bd080249e1369eb66b1833cb9afb355a9ab7ee79
Josh Long <josh@litprotocol.com>
2023-07-18 13:41:07 -0400

    add configuration test back

--- packages/getlit-sdk/src/lib/getlit-sdk.spec.ts
+++ packages/getlit-sdk/src/lib/getlit-sdk.spec.ts
@@ -25,6 +25,29 @@ describe('getlitSDK', () => {
         // -- verify lit is ready
         expect(globalThis.Lit.ready).toBe(true);
       });
+      it('should build', async () => {
+        await globalThis.Lit.builder?.build();
+
+        // -- verify lit is ready
+        expect(globalThis.Lit.ready).toBe(true);
+      });
+
+      it('should emit "foo" event and return "bar"', async () => {
+        const mockFn = jest.fn();
+
+        if (!globalThis.Lit.eventEmitter) {
+          throw new Error('globalThis.Lit.eventEmitter is undefined!');
+        }
+
+        // -- start listening for "foo" event
+        globalThis.Lit.eventEmitter.on('foo', mockFn);
+
+        // -- now emit "foo" event, the mockFn should be called with "bar"
+        globalThis.Lit.eventEmitter.emit('foo', 'bar');
+
+        // -- verify mockFn was called with "bar"
+        expect(mockFn).toHaveBeenCalledWith('bar');
+      });
     });
 
     describe('with contract options', () => {
@@ -49,21 +72,23 @@ describe('getlitSDK', () => {
   });
 
   it('should emit ready event and configure custom option', async () => {
-    globalThis.Lit.builder?.withContractOptions({
-      signer: new PKPEthersWallet({
-        pkpPubKey: LITCONFIG.PKP_PUBKEY,
-        rpc: LITCONFIG.CHRONICLE_RPC,
-        controllerAuthSig: LITCONFIG.CONTROLLER_AUTHSIG,
-      }),
+    globalThis.Lit.eventEmitter?.on('ready', async () => {
+      globalThis.Lit.builder?.withContractOptions({
+        signer: new PKPEthersWallet({
+          pkpPubKey: LITCONFIG.PKP_PUBKEY,
+          rpc: LITCONFIG.CHRONICLE_RPC,
+          controllerAuthSig: LITCONFIG.CONTROLLER_AUTHSIG,
+        }),
+      });
+      await globalThis.Lit.builder?.build();
+  
+      expect(globalThis.Lit).toBeDefined();
+      expect(globalThis.Lit.instance).toBeDefined();
+      expect(
+        (globalThis.Lit.instance as any)['_options']?.signer
+      ).toBeDefined();
+      expect(globalThis.Lit.nodeClient).toBeDefined();
     });
-    await globalThis.Lit.builder?.build();
-
-    expect(globalThis.Lit).toBeDefined();
-    expect(globalThis.Lit.instance).toBeDefined();
-    expect(
-      (globalThis.Lit.instance as any)['_options']?.signer
-    ).toBeDefined();
-    expect((globalThis.Lit.eventEmitter as any)['_eventsCount']).toBe(1);
   }, 10_000);
 
   it('Should sign message', async () => {
@@ -72,7 +97,7 @@ describe('getlitSDK', () => {
       accountPublicKey: LITCONFIG.PKP_PUBKEY,
       signingMaterial: 'Hello World',
       credentials: [],
-      authMatrial: LITCONFIG.CONTROLLER_AUTHSIG,
+      authMaterial: LITCONFIG.CONTROLLER_AUTHSIG,
     });
     expect(sig).toBeDefined();
   }, 10_000);
@@ -101,4 +126,5 @@ describe('getlitSDK', () => {
     expect(enctyptedContent).toBeDefined();
     expect(globalThis.Lit?.storage?.getItem(enctyptedContent.storageKey)).toBeDefined();
   }, 100_000);
+  
 });
