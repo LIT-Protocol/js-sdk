58650793ca0ac98ada1c16aee4aff97eb6532f74
Josh Long <josh@litprotocol.com>
2023-09-08 18:05:40 -0400

    add connect call to claimKeyId

--- packages/lit-auth-client/src/lib/providers/BaseProvider.ts
+++ packages/lit-auth-client/src/lib/providers/BaseProvider.ts
@@ -189,6 +189,12 @@ export abstract class BaseProvider {
    * @returns {Promise<ClaimKeyResponse>} - Response from the network for the claim
    */
   public async claimKeyId<T=ClaimProcessor>(claimRequest: ClaimRequest<T>): Promise<ClaimKeyResponse> {
+      if (this.litNodeClient.ready) {
+        await this.litNodeClient.connect().catch((err) => {
+          throw err; // throw error up to caller
+        });
+      }
+      
       const res = await this.litNodeClient.claimKeyId<T>(claimRequest);
       return res;
   }
