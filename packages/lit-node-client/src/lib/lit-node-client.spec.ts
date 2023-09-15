c05f62b894d9a9f27d8040696a48da9ddeb5c075
Josh Long <josh@litprotocol.com>
2023-09-15 14:19:48 -0400

    add claim combiner test

--- packages/lit-node-client/src/lib/lit-node-client.spec.ts
+++ packages/lit-node-client/src/lib/lit-node-client.spec.ts
@@ -106,13 +106,50 @@ describe('Lit Actions', () => {
     expect(sig.publicKey).toBeDefined();
   });
 
+  it('Should combine claim responses', () => {
+    const nodeclaimResponses = [
+      {
+        'foo':       {
+          keyId: "abc1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        },
+        bar:       {
+          keyId: "xyz1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        }
+      },
+      {
+        'foo':       {
+          keyId: "abc1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        },
+        bar:       {
+          keyId: "xyz1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        }
+      },
+      {
+        'foo':       {
+          keyId: "abc1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        },
+        bar:       {
+          keyId: "xyz1234",
+          signature: "f84ad3d3efa42abcae2b3567c836f1342552a75ed038e9403b77a7c47e3500242572c86ddee7f9af4994793a741111553ac7652897ee5447b143b8067557a3511b"
+        }
+      }
+    ];
 
+    const combinedClaims = client.getClaims(nodeclaimResponses);
+    expect(Object.keys(combinedClaims).length).toEqual(2);
+    expect(combinedClaims['foo'].signatures.length).toEqual(3);
+  });
   it('should claim key id from auth method', async () => {
     const authMethod = {
       authMethodType: 4,
       accessToken: LITCONFIG.AUTH_METHOD_ACCESS_TOKEN
     };
-    let res = await client.claimKeyId<RelayClaimProcessor>({
+    let res = await client.claimKeyId({
       authMethod
     });
     
