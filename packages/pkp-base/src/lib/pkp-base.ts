0734ff3f36d3a9621a408839dfbbc427957b1727
Ansonhkg <ansonox@gmail.com>
2023-09-20 18:05:44 +0100

    fix: min nodes

--- packages/pkp-base/src/lib/pkp-base.ts
+++ packages/pkp-base/src/lib/pkp-base.ts
@@ -103,7 +103,7 @@ export class PKPBase<T = PKPBaseDefaultParams> {
       minNodeCount:
         prop.bootstrapUrls && prop.litNetwork == 'custom'
           ? prop.minNodeCount
-          : 6,
+          : 2,
     });
   }
 
