483c950a82f2b5acbfa139e203c00bb64cfc7323
Ansonhkg <ansonox@gmail.com>
2023-09-21 16:26:12 +0100

    chore: indentation

--- tools/scripts/tools.mjs
+++ tools/scripts/tools.mjs
@@ -1424,8 +1424,8 @@ async function validateDependencyVersions() {
     greenLog(
       `
     ❗️ Before publishing, make sure you have tested the build!
-      - yarn test:unit       | run unit tests
-      - yarn test:e2e        | run e2e tests on browser
+      - yarn test:unit     | run unit tests
+      - yarn test:e2e      | run e2e tests on browser
       - yarn test:e2e:node | run e2e tests on nodejs
       `,
       true
