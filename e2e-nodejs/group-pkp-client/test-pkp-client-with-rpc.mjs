33c6c758399050dd030cf2bde6a5b7ba329f61de
Ansonhkg <ansonox@gmail.com>
2023-09-21 21:34:39 +0100

    fix: pkp client works too

--- e2e-nodejs/group-pkp-client/test-pkp-client-with-rpc.mjs
+++ e2e-nodejs/group-pkp-client/test-pkp-client-with-rpc.mjs
@@ -3,6 +3,7 @@ import { success, fail, testThis } from '../../tools/scripts/utils.mjs';
 import LITCONFIG from '../../lit.config.json' assert { type: 'json' };
 import { client } from '../00-setup.mjs';
 import { PKPClient } from '@lit-protocol/pkp-client';
+import { ethers } from 'ethers';
 
 export async function main() {
   // ==================== Setup ====================
@@ -20,17 +21,48 @@ export async function main() {
 
   // ==================== Test Logic ====================
   const PAYLOAD = {
-    to: PKP_ETH_ADDRESS,
+    to: LITCONFIG.PKP_ETH_ADDRESS,
     value: 0,
     data: '0x',
   };
 
-  const signature = await pkpClient.getEthWallet().signTransaction(PAYLOAD);
+  const rawSignedTx = await pkpClient.getEthWallet().signTransaction(PAYLOAD);
 
+  const parsedTransaction = ethers.utils.parseTransaction(rawSignedTx);
+
+  const signature = ethers.utils.joinSignature({
+    r: parsedTransaction.r,
+    s: parsedTransaction.s,
+    v: parsedTransaction.v,
+  });
+
+  const rawTx = {
+    nonce: parsedTransaction.nonce,
+    gasPrice: parsedTransaction.gasPrice,
+    gasLimit: parsedTransaction.gasLimit,
+    to: parsedTransaction.to,
+    value: parsedTransaction.value,
+    data: parsedTransaction.data,
+    chainId: parsedTransaction.chainId, // Include chainId if the transaction is EIP-155
+  };
+
+  const txHash = ethers.utils.keccak256(
+    ethers.utils.serializeTransaction(rawTx)
+  );
+
+  const { v, r, s } = parsedTransaction;
+
+  const recoveredAddress = ethers.utils.recoverAddress(txHash, { r, s, v });
   // ==================== Post-Validation ====================
 
   if (signature.length !== 132) {
     return fail('signature should be defined');
+  }
+
+  if (recoveredAddress !== LITCONFIG.PKP_ETH_ADDRESS) {
+    return fail(
+      `recoveredAddres should be ${LITCONFIG.PKP_ETH_ADDRESS}, got ${recoveredAddress}`
+    );
   }
 
   // ==================== Success ====================
