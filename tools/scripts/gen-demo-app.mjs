0a69ae975a45062ffe45652ac9b2e7e8ba0788e5
Ansonhkg <ansonox@gmail.com>
2023-10-12 00:29:20 +0100

    feat: script to generate demo app

---
+++ tools/scripts/gen-demo-app.mjs
@@ -0,0 +1,54 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { redLog } from './utils.mjs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * This script clones the nextjs-demo-template directory, allows the user to set a new name for the project,
+ * and changes the package.json name accordingly.
+ *
+ * Usage: node gen-demo-app.mjs --name=<name>
+ */
+
+// Parse arguments
+const args = process.argv.slice(2);
+const NEW_NAME = args.find((arg) => arg.startsWith('--name'))?.split('=')[1];
+if (!NEW_NAME) {
+  redLog('Please provide a new name for the project using --name=<name>');
+  process.exit(1);
+}
+
+// Define directories
+const TEMPLATE_DIR = path.join(__dirname, 'nextjs-demo-template');
+const APPS_DIR = path.join(process.cwd(), 'apps'); // Changed from __dirname to process.cwd()
+const NEW_PROJECT_DIR = path.join(APPS_DIR, NEW_NAME);
+
+// Check if directory already exists
+if (fs.existsSync(NEW_PROJECT_DIR)) {
+  redLog(
+    `The directory ${NEW_PROJECT_DIR} already exists. Please choose a different name.`
+  );
+  process.exit(1);
+}
+
+// Clone template to new project directory
+execSync(`cp -r ${TEMPLATE_DIR} ${NEW_PROJECT_DIR}`);
+
+// Update package.json name
+const packageJsonPath = path.join(NEW_PROJECT_DIR, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+packageJson.name = NEW_NAME;
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+// Run yarn install in the project directory
+console.log(`\n📝 Available commands:\n\n`);
+console.log(`Project ${NEW_NAME} has been created in the ./apps directory.`);
+console.log(
+  `You can start the project by navigating to ./apps/${NEW_NAME} and running yarn install and then yarn dev.\n\n`
+);
+
+process.exit();
