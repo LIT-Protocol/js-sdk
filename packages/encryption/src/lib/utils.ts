0111877450815988a9534117088192c823889ec3
Adarsh-Kumar28 <adarsh@litprotocol.com>
2023-10-16 22:28:49 +0530

    refac: Remove logs & manual test

--- packages/encryption/src/lib/utils.ts
+++ packages/encryption/src/lib/utils.ts
@@ -7,9 +7,7 @@ import {
 
 function isTokenOperator(token: any): boolean {
   const OPERATORS = ["and", "or"]; // Only permissible boolean operators on the nodes
-  const res = token.hasOwnProperty("operator") && OPERATORS.includes(token.operator);
-  console.log('isTokenOperator- ', isTokenOperator);
-  return res;
+  return token.hasOwnProperty("operator") && OPERATORS.includes(token.operator);
 }
 
 export function isValidBooleanExpression(
@@ -18,39 +16,29 @@ export function isValidBooleanExpression(
   | SolRpcConditions 
   | UnifiedAccessControlConditions
 ): boolean {
-  console.log('expression');
-  console.log(expression);
   const STATES = { START: "start", CONDITION: "condition", OPERATOR: "operator" };
 
   let currentState = STATES.START;
   for (const token of expression) {
-    console.log('token- ', token);
     switch (currentState) {
       case STATES.START:
       case STATES.OPERATOR:
-        console.log(currentState);
         if (isTokenOperator(token)) {
-          console.log('invalid- 1');
           return false;
         }
         // Nested conditions expression
         if (Array.isArray(token) && !isValidBooleanExpression(token)) {
-          console.log('invalid- 2');
           return false;
         }
         currentState = STATES.CONDITION;
         break;
       default:
-        console.log('STATES.CONDITION');
         if (!isTokenOperator(token)) {
-          console.log('invalid- 3');
           return false;
         }
         currentState = STATES.OPERATOR;
     }
   }
 
-  const res = currentState === STATES.CONDITION;
-  console.log('isValidBooleanExpression- ', res);
-  return res;
+  return currentState === STATES.CONDITION;
 }
