42a78a94178552308e345a3230cfef37fe304627
debie <debbs.ly@gmail.com>
2023-10-17 14:14:46 -0700

    Revert "Update example, now running"
    This reverts commit 9e5aa6dc35f0c1d6dc3163b55a3010e64cc476e4.

--- apps/demo-pkp-social-auth-next-ts/next.config.js
+++ apps/demo-pkp-social-auth-next-ts/next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-var webpack = require('webpack');
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.plugins.push(
-      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-        resource.request = resource.request.replace(/^node:/, "");
-      })
-    );
-    return config;
-  },
 }
 
 module.exports = nextConfig
