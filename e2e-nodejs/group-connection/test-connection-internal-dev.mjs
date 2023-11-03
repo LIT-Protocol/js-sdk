5c71876a98ee4f655d53cf79d22a2bbc9c4f742d
Ansonhkg <ansonox@gmail.com>
2023-11-03 11:34:56 +0000

    feat: add test

---
+++ e2e-nodejs/group-connection/test-connection-internal-dev.mjs
@@ -0,0 +1,27 @@
+import path from 'path';
+import { success, fail, testThis } from '../../tools/scripts/utils.mjs';
+import LITCONFIG from '../../lit.config.json' assert { type: 'json' };
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+
+const LIT_NETWORK = 'cayenne';
+
+export async function main() {
+  // ==================== Test Logic ====================
+  const client = new LitNodeClient({
+    litNetwork: 'internalDev'
+  });
+  await client.connect();
+
+  // ==================== Post-Validation ====================
+  if (!client.ready) {
+    return fail('client not ready');
+  }
+  if (client.config.litNetwork !== LIT_NETWORK) {
+    return fail(`client not connected to ${LIT_NETWORK}`);
+  }
+
+  // ==================== Success ====================
+  return success(`Connected to ${LIT_NETWORK}`);
+}
+
+await testThis({ name: path.basename(import.meta.url), fn: main });
