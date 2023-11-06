ab7e2686bd3faf0fbebb015930ad82e42e6467e0
Josh Long <josh@litprotocol.com>
2023-11-06 09:46:24 -0500

    update contract gen

---
+++ packages/contracts-sdk/src/abis/PKPNFTMetadata.sol/PKPNFTMetadataContract.js
@@ -0,0 +1,8 @@
+import { ethers } from "ethers";
+import { PKPNFTMetadataData } from "./PKPNFTMetadataData.js";
+
+export const getPKPNFTMetadataContract = (provider) => new ethers.Contract(
+  PKPNFTMetadataData.address,
+  PKPNFTMetadataData.abi,
+  provider
+);
