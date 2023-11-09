7acb06824be773350c330e671902006eb0ba8721
Anson <ansonox@gmail.com>
2023-11-09 15:05:31 +0000

    feat: ability to add scope when minting (#251)

---
+++ packages/lit-auth-client/src/lib/validators.spec.ts
@@ -0,0 +1,106 @@
+import { validateMintRequestBody } from './validators';
+
+
+describe('validateMintRequestBody', () => {
+  const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should pass validation when all fields are correct and present', () => {
+    const customArgs = {
+      keyType: 2,
+      permittedAuthMethodTypes: [1],
+      permittedAuthMethodIds: ['id123'],
+      permittedAuthMethodPubkeys: ['pubkey123'],
+      permittedAuthMethodScopes: [[1]],
+      addPkpEthAddressAsPermittedAddress: true,
+      sendPkpToItself: true,
+    };
+    expect(validateMintRequestBody(customArgs)).toBe(true);
+    expect(mockConsoleError).not.toHaveBeenCalled();
+  });
+
+  it('should pass validation when no fields are provided', () => {
+    const customArgs = {};
+    expect(validateMintRequestBody(customArgs)).toBe(true);
+    expect(mockConsoleError).not.toHaveBeenCalled();
+  });
+
+  it('should pass validation when some fields are provided and correct', () => {
+    const customArgs = {
+      keyType: 2,
+      permittedAuthMethodPubkeys: ['pubkey123'],
+    };
+    expect(validateMintRequestBody(customArgs)).toBe(true);
+    expect(mockConsoleError).not.toHaveBeenCalled();
+  });
+
+  it('should fail validation and log error for incorrect keyType', () => {
+    const customArgs = {
+      keyType: '2', // should be a number
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for keyType'));
+  });
+
+  it('should fail validation and log error for incorrect permittedAuthMethodTypes', () => {
+    const customArgs = {
+      permittedAuthMethodTypes: ['1'], // should be an array of numbers
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for permittedAuthMethodTypes'));
+  });
+
+  it('should fail validation and log error for incorrect permittedAuthMethodIds', () => {
+    const customArgs = {
+      permittedAuthMethodIds: [123], // should be an array of strings
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for permittedAuthMethodIds'));
+  });
+
+  it('should fail validation and log error for incorrect permittedAuthMethodPubkeys', () => {
+    const customArgs = {
+      permittedAuthMethodPubkeys: [123], // should be an array of strings
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for permittedAuthMethodPubkeys'));
+  });
+  it('should fail validation and log error for incorrect permittedAuthMethodScopes', () => {
+    const customArgs = {
+      permittedAuthMethodScopes: [[1]], // should be an array of ethers.BigNumber
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(true);
+  });
+
+  it('should fail validation and log error for incorrect addPkpEthAddressAsPermittedAddress', () => {
+    const customArgs = {
+      addPkpEthAddressAsPermittedAddress: 'true', // should be a boolean
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for addPkpEthAddressAsPermittedAddress'));
+  });
+
+  it('should fail validation and log error for incorrect sendPkpToItself', () => {
+    const customArgs = {
+      sendPkpToItself: 'true', // should be a boolean
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid type for sendPkpToItself'));
+  });
+
+  it('should fail validation and log error for extraneous keys', () => {
+    const customArgs = {
+      extraneousKey: 'unexpected', // This key is not defined in MintRequestBody
+    };
+    expect(validateMintRequestBody(customArgs as any)).toBe(false);
+    expect(mockConsoleError).toHaveBeenCalledWith(expect.stringContaining('Invalid key found: extraneousKey'));
+  });
+
+});
---
+++ packages/lit-auth-client/src/lib/validators.ts
@@ -0,0 +1,51 @@
+import { MintRequestBody } from "@lit-protocol/types";
+
+export const validateMintRequestBody = (customArgs: Partial<MintRequestBody>): boolean => {
+  let isValid = true;
+  const validKeys = ['keyType', 'permittedAuthMethodTypes', 'permittedAuthMethodIds', 'permittedAuthMethodPubkeys', 'permittedAuthMethodScopes', 'addPkpEthAddressAsPermittedAddress', 'sendPkpToItself'];
+
+  // Check for any extraneous keys
+  for (const key of Object.keys(customArgs)) {
+    if (!validKeys.includes(key)) {
+      console.error(`Invalid key found: ${key}. This key is not allowed. Valid keys are: ${validKeys.join(', ')}`);
+      isValid = false;
+    }
+  }
+
+  if (customArgs.keyType !== undefined && typeof customArgs.keyType !== 'number') {
+    console.error('Invalid type for keyType: expected a number.');
+    isValid = false;
+  }
+
+  if (customArgs.permittedAuthMethodTypes !== undefined && (!Array.isArray(customArgs.permittedAuthMethodTypes) || !customArgs.permittedAuthMethodTypes.every(type => typeof type === 'number'))) {
+    console.error('Invalid type for permittedAuthMethodTypes: expected an array of numbers.');
+    isValid = false;
+  }
+
+  if (customArgs.permittedAuthMethodIds !== undefined && (!Array.isArray(customArgs.permittedAuthMethodIds) || !customArgs.permittedAuthMethodIds.every(id => typeof id === 'string'))) {
+    console.error('Invalid type for permittedAuthMethodIds: expected an array of strings.');
+    isValid = false;
+  }
+
+  if (customArgs.permittedAuthMethodPubkeys !== undefined && (!Array.isArray(customArgs.permittedAuthMethodPubkeys) || !customArgs.permittedAuthMethodPubkeys.every(pubkey => typeof pubkey === 'string'))) {
+    console.error('Invalid type for permittedAuthMethodPubkeys: expected an array of strings.');
+    isValid = false;
+  }
+
+  if (customArgs.permittedAuthMethodScopes !== undefined && (!Array.isArray(customArgs.permittedAuthMethodScopes) || !customArgs.permittedAuthMethodScopes.every(scope => Array.isArray(scope) && scope.every(s => typeof s === 'number')))) {
+    console.error('Invalid type for permittedAuthMethodScopes: expected an array of arrays of numberr.');
+    isValid = false;
+  }
+
+  if (customArgs.addPkpEthAddressAsPermittedAddress !== undefined && typeof customArgs.addPkpEthAddressAsPermittedAddress !== 'boolean') {
+    console.error('Invalid type for addPkpEthAddressAsPermittedAddress: expected a boolean.');
+    isValid = false;
+  }
+
+  if (customArgs.sendPkpToItself !== undefined && typeof customArgs.sendPkpToItself !== 'boolean') {
+    console.error('Invalid type for sendPkpToItself: expected a boolean.');
+    isValid = false;
+  }
+
+  return isValid;
+};
