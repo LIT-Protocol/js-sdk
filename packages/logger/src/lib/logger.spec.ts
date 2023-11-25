d3da82c7cb1707167bada953200e7684779975d2
Josh Long <josh@litprotocol.com>
2023-11-25 10:01:09 -0500

    add tests

--- packages/logger/src/lib/logger.spec.ts
+++ packages/logger/src/lib/logger.spec.ts
@@ -1,7 +1,61 @@
-import { logger } from './logger';
+import { Logger, LogLevel, LogManager } from './logger';
 
 describe('logger', () => {
-  it('should work', () => {
-    expect(logger()).toEqual('logger');
+  let lm: LogManager;
+  beforeEach(() => {
+    LogManager.clearInstance();
+    lm = LogManager.Instance;
+  });
+
+  it('Log Manager singleton should be defined', () => {
+    expect(typeof lm).toEqual("object");
+  });
+
+  it('should make logger with category', () => {
+    const logger = lm.get('category');
+    expect(logger.category).toEqual('category');
+  });
+  
+  it ('should make logger with id and category', () => {
+    const logger = lm.get('category', 'foo');
+    expect(logger.id).toEqual('foo');
+  });
+
+  it('Log Manager should pass config to loggers', () => {
+    lm.withConfig({
+      "condenseLogs": true
+    });
+    let logger = lm.get('category');
+    expect(logger.Config?.['condenseLogs']).toEqual(true);
+  });
+
+  it('Hashing enabled should filter non unique logs', () => {
+    lm.withConfig({
+      "condenseLogs": true
+    });
+    let logger = lm.get('category', 'bar');
+    expect(logger.Config?.['condenseLogs']).toEqual(true);
+    logger.info("hello");
+    logger.info("hello");
+    let logs = lm.getLogsForId('bar');
+    expect(logs.length).toEqual(1);
+  });
+
+  it('should respect info logging level', () => {
+    const logger = lm.get('info-logger', 'foo');
+    logger.setLevel(LogLevel.INFO);
+    logger.info('logging');
+    logger.debug('shouldnt log');
+    let logs = lm.getLogsForId('foo');
+    expect(logs.length).toEqual(1);
+  });
+
+  it('should log error at any level', () => {
+    const logger = lm.get('info-logger', 'foo2');
+    logger.setLevel(LogLevel.DEBUG);
+    logger.debug('logging');
+    logger.error("error");
+    let logs = lm.getLogsForId('foo2');
+    expect(logs.length).toEqual(2); 
   });
 });
