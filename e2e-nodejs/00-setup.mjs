855b9bcbff43621f5e167846bb347d32c1c9d67a
Josh Long <josh@litprotocol.com>
2023-12-01 17:44:39 -0500

    add comments

--- e2e-nodejs/00-setup.mjs
+++ e2e-nodejs/00-setup.mjs
@@ -1,10 +1,11 @@
-import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import { LitNodeClient, uint8arrayFromString } from '@lit-protocol/lit-node-client';
 import LITCONFIG from '../lit.config.json' assert { type: 'json' };
 import { fail } from '../tools/scripts/utils.mjs';
 import {LitContracts} from "@lit-protocol/contracts-sdk";
 import {ethers} from "ethers";
 import * as siwe from 'siwe';
 
+// ==================== ENV Loading ====================
 const network = process.env.NETWORK ?? LITCONFIG.TEST_ENV.litNetwork;
 const debug = process.env.DEBUG === 'true' ?? LITCONFIG.TEST_ENV.debug;
 const minNodeCount = LITCONFIG.TEST_ENV.minNodeCount;
@@ -27,9 +28,11 @@ if (LITCONFIG.CONTROLLER_AUTHSIG === undefined) {
   fail('Controller authSig cannot be empty');
 }
 
+// ==================== SIWE Gen ====================
 const provider = new ethers.providers.JsonRpcProvider(
   LITCONFIG.CHRONICLE_RPC
 );
+
 const wallet = new ethers.Wallet(LITCONFIG.CONTROLLER_PRIVATE_KEY, provider);
 const address = ethers.utils.getAddress(await wallet.getAddress());
 
@@ -60,29 +63,17 @@ const authSig = {
 };
 console.log("generated siwe for test run: ", authSig);
 
+// ==================== Global Vars ====================
 globalThis.LitCI = {};
 globalThis.LitCI.network = network;
 globalThis.LitCI.debug = debug;
 globalThis.LitCI.sevAttestation = checkSevAttestation;
 globalThis.LitCI.CONTROLLER_AUTHSIG = authSig;
+globalThis.LitCI.CONTROLLER_AUTHSIG_2 = LITCONFIG.CONTROLLER_AUTHSIG_2;
 
 if (network === 'cayenne') {
   globalThis.LitCI.PKP_INFO = {};
   globalThis.LitCI.PKP_INFO.publicKey = LITCONFIG.PKP_PUBKEY;
-  globalThis.LitCI.CONTROLLER_AUTHSIG_2 = LITCONFIG.CONTROLLER_AUTHSIG_2;
-  let contractClient = new LitContracts({
-    signer: wallet,
-    debug: process.env.DEBUG === 'true' ?? LITCONFIG.TEST_ENV.debug,
-    network: process.env.NETWORK ?? LITCONFIG.TEST_ENV.litNetwork, 
-  });
-  await contractClient.connect();
-  let tokenId = await contractClient.pkpPermissionsContract.read.getTokenIdsForAuthMethod(
-    1,
-    ethers.utils.keccak256(`${globalThis.LitCI.CONTROLLER_AUTHSIG_2.address}:lit`)
-  );
-
-  let pubKey = await contractClient.pkpPermissionsContract.read.getPubkey(tokenId);
-  globalThis.LitCI.PKP_INFO.publicKey2 = pubKey;
 } else if (network === 'internalDev') {  
   let contractClient = new LitContracts({
     signer: wallet,
