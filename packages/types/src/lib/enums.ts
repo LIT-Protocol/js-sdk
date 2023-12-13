018eb38cf63dd9b510b698418015565f24ba183b
Josh Long <josh@litprotocol.com>
2023-12-13 14:15:25 -0500

    update enum

--- packages/types/src/lib/enums.ts
+++ packages/types/src/lib/enums.ts
@@ -4,11 +4,20 @@ export enum IRelayAuthStatus {
   Failed = 'Failed',
 }
 
+/**
+ * Supported PKP auth method types
+ */
 export enum AuthMethodType {
   EthWallet = 1,
-  LitAction,
-  WebAuthn,
-  Discord,
-  Google,
-  GoogleJwt,
+  LitAction = 2,
+  WebAuthn = 3,
+  Discord = 4,
+  Google = 5,
+  GoogleJwt = 6,
+  AppleJwt = 8,
+  StytchOtp = 9,
+  StytchEmailFactorOtp = 10,
+  StytchSmsFactorOtp = 11,
+  StytchWhatsAppFactorOtp = 12,
+  StytchTotpFactorOtp = 13,
 }
