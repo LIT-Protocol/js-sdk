6cc7c6e7164eaa26d2bdcb1cf9d700722590f322
Ansonhkg <ansonox@gmail.com>
2024-01-12 03:37:45 +0300

    add sanitize

--- e2e-nodejs/group-recap/test-recap-from-lit-node-client.mjs
+++ e2e-nodejs/group-recap/test-recap-from-lit-node-client.mjs
@@ -7,55 +7,12 @@
 import path from 'path';
 import { success, fail, testThis } from '../../tools/scripts/utils.mjs';
 import LITCONFIG from '../../lit.config.json' assert { type: 'json' };
-// import { client } from '../00-setup.mjs';
-import { RecapSessionCapabilityObject } from '@lit-protocol/auth-helpers';
 import { LitNodeClient } from '@lit-protocol/lit-node-client';
-import {
-  hashResourceIdForSigning,
-  hashUnifiedAccessControlConditions,
-} from '@lit-protocol/access-control-conditions';
-import {
-  LitAbility,
-  LitAccessControlConditionResource,
-  LitRLIResource,
-  LitActionResource,
-} from '@lit-protocol/auth-helpers';
-
-import { generateUnifiedAccsForRLIDelegation } from '@lit-protocol/access-control-conditions';
+import { LitAbility, LitRLIResource } from '@lit-protocol/auth-helpers';
 
 import { LitContracts } from '@lit-protocol/contracts-sdk';
 import { ethers } from 'ethers';
 import * as siwe from 'siwe';
-import * as LitJsSdk from '@lit-protocol/lit-node-client';
-
-// const getStandardAuthSigh = async (wallet) => {
-//   const address = wallet.address;
-
-//   // siwe message
-//   const domain = 'localhost';
-//   const origin = 'https://localhost/login';
-//   const statement =
-//     'This is a test statement.  You can put anything you want here.';
-//   const siweMessage = new siwe.SiweMessage({
-//     domain,
-//     address: address,
-//     statement,
-//     uri: origin,
-//     version: '1',
-//     chainId: 1,
-//     expirationTime: new Date(Date.now() + 60 * 60 * 1000).toISOString(),
-//   });
-//   const messageToSign = siweMessage.prepareMessage();
-
-//   const signature = await wallet.signMessage(messageToSign);
-
-//   return {
-//     sig: signature,
-//     derivedVia: 'web3.eth.personal.sign',
-//     signedMessage: messageToSign,
-//     address: address,
-//   };
-// };
 
 export async function main() {
   if (process.env.LOAD_ENV === 'false') {
@@ -64,21 +21,17 @@ export async function main() {
   }
 
   // ==================== Setup ====================
-  const delegatedWalletA = globalThis.LitCI.wallet;
-  const delegatedWalletA_address = globalThis.LitCI.wallet.address;
-  const delegateWalletA_authSig = globalThis.LitCI.CONTROLLER_AUTHSIG;
-  const delegatedWalletA_pkpPublicKey = globalThis.LitCI.PKP_INFO.publicKey;
+  const dAppOwnerWallet = globalThis.LitCI.wallet;
+  const dAppOwnerWallet_address = globalThis.LitCI.wallet.address;
+  const dAppOwnerWallet_authSig = globalThis.LitCI.CONTROLLER_AUTHSIG;
+  const dAppOwnerWallet_pkpPublicKey = globalThis.LitCI.PKP_INFO.publicKey;
 
-  console.log('delegateWalletA_authSig:', delegateWalletA_authSig);
-  console.log('delegatedWalletA_address:', delegatedWalletA_address);
-  console.log('delegatedWalletA_pkpPublicKey:', delegatedWalletA_pkpPublicKey);
+  console.log('dAppOwnerWallet_authSig:', dAppOwnerWallet_authSig);
+  console.log('dAppOwnerWallet_address:', dAppOwnerWallet_address);
+  console.log('dAppOwnerWallet_pkpPublicKey:', dAppOwnerWallet_pkpPublicKey);
 
   const delegatedWalletB = new ethers.Wallet.createRandom();
   const delegatedWalletB_address = delegatedWalletB.address;
-
-  const rliTokenIdMock = '41';
-  // const delegateWalletB_authSig = await getStandardAuthSigh(delegatedWalletB);
-  // console.log('delegatedWalletB_address:', delegatedWalletB_address);
 
   // ====================================================
   // =                    dAPP OWNER                    =
@@ -91,19 +44,20 @@ export async function main() {
 
   // -- 2. minting RLI
   let contractClient = new LitContracts({
-    signer: delegatedWalletA,
+    signer: dAppOwnerWallet,
     debug: process.env.DEBUG === 'true' ?? LITCONFIG.TEST_ENV.debug,
     network: process.env.NETWORK ?? LITCONFIG.TEST_ENV.litNetwork,
   });
 
   await contractClient.connect();
 
-  // const { rliTokenIdStr } = await contractClient.mintRLI({
-  //   requestsPerDay: 14400, // 10 request per minute
-  //   daysUntilUTCMidnightExpiration: 2,
-  // });
+  // const rliTokenIdStr = '11';
+  const { rliTokenIdStr } = await contractClient.mintRLI({
+    requestsPerDay: 14400, // 10 request per minute
+    daysUntilUTCMidnightExpiration: 2,
+  });
 
-  // console.log('rliTokenIdStr:', rliTokenIdStr);
+  console.log('rliTokenIdStr:', rliTokenIdStr);
 
   const client = new LitNodeClient({
     litNetwork: process.env.NETWORK ?? LITCONFIG.TEST_ENV.litNetwork,
@@ -116,10 +70,9 @@ export async function main() {
 
   const { rliDelegationAuthSig, litResource } =
     await client.createRliDelegationAuthSig({
-      dAppOwnerWallet: delegatedWalletA,
-      // rliTokenId: rliTokenIdStr,
-      rliTokenId: rliTokenIdMock,
-      addresses: [delegatedWalletA_address, delegatedWalletB_address],
+      dAppOwnerWallet: dAppOwnerWallet,
+      rliTokenId: rliTokenIdStr,
+      addresses: [dAppOwnerWallet_address, delegatedWalletB_address],
     });
 
   console.log('rliDelegationAuthSig:', rliDelegationAuthSig);
@@ -136,7 +89,7 @@ export async function main() {
 
     const message = new siwe.SiweMessage({
       domain: 'example.com',
-      address: delegatedWalletA_address,
+      address: dAppOwnerWallet_address,
       statement: 'Sign a session key to use with Lit Protocol',
       uri,
       version: '1',
@@ -145,13 +98,13 @@ export async function main() {
       resources,
     });
     const toSign = message.prepareMessage();
-    const signature = await delegatedWalletA.signMessage(toSign);
+    const signature = await dAppOwnerWallet.signMessage(toSign);
 
     const authSig = {
       sig: signature,
       derivedVia: 'web3.eth.personal.sign',
       signedMessage: toSign,
-      address: delegatedWalletA_address,
+      address: dAppOwnerWallet_address,
     };
 
     return authSig;
@@ -162,7 +115,7 @@ export async function main() {
     chain: 'ethereum',
     resourceAbilityRequests: [
       {
-        resource: new LitRLIResource(rliTokenIdMock),
+        resource: new LitRLIResource(rliTokenIdStr),
         ability: LitAbility.RateLimitIncreaseAuth,
       },
     ],
@@ -185,7 +138,7 @@ export async function main() {
   //   ]
   // }
   const res = await client.executeJs({
-    // authSig: delegateWalletA_authSig,
+    // authSig: globalThis.LitCI.CONTROLLER_AUTHSIG,
     sessionSigs: sessionSigs,
     code: `(async () => {
       const sigShare = await LitActions.signEcdsa({
@@ -199,7 +152,7 @@ export async function main() {
       dataToSign: ethers.utils.arrayify(
         ethers.utils.keccak256([1, 2, 3, 4, 5])
       ),
-      publicKey: delegatedWalletA_pkpPublicKey,
+      publicKey: dAppOwnerWallet_pkpPublicKey,
     },
   });
 
