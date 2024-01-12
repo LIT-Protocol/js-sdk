07cfc8ffc7cc8cc51b887e43e1b49167d3db344f
FedericoAmura <federicoamura@gmail.com>
2024-01-12 20:10:09 -0300

    add bls encryption and decryption e2e testS

---
+++ e2e-nodejs/group-pkp-encryption-decryption/test-pkp-encryption-decryption-string.mjs
@@ -0,0 +1,56 @@
+import path from 'path';
+import * as LitJsSdk from '@lit-protocol/lit-node-client';
+import { success, fail, testThis } from '../../tools/scripts/utils.mjs';
+import { client } from '../00-setup.mjs';
+
+export async function main() {
+  // ==================== Setup ====================
+  const chain = 'ethereum';
+  const accessControlConditions = [
+    {
+      contractAddress: '',
+      standardContractType: '',
+      chain,
+      method: 'eth_getBalance',
+      parameters: [':userAddress', 'latest'],
+      returnValueTest: {
+        comparator: '>=',
+        value: '0',
+      },
+    },
+  ];
+  const message = 'Hello world';
+
+  // ==================== Test Logic ====================
+  const { ciphertext, dataToEncryptHash } = await LitJsSdk.encryptString(
+    {
+      accessControlConditions,
+      authSig: globalThis.LitCI.CONTROLLER_AUTHSIG,
+      chain,
+      dataToEncrypt: message,
+    },
+    client,
+  );
+  const decryptedMessage = await LitJsSdk.decryptToString(
+    {
+      accessControlConditions,
+      ciphertext,
+      dataToEncryptHash,
+      authSig: globalThis.LitCI.CONTROLLER_AUTHSIG,
+      chain,
+    },
+    client,
+  );
+
+  // ==================== Post-Validation ====================
+  if (message !== decryptedMessage) {
+    return fail(
+      `decryptedMessage should be ${message} but received ${decryptedMessage}`
+    );
+  }
+
+  // ==================== Success ====================
+  return success('Message was encrypted and then decrypted successfully');
+}
+
+await testThis({ name: path.basename(import.meta.url), fn: main });
