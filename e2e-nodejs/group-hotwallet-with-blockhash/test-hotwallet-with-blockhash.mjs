1365956c2b72a6a056bc0dd47ac8d0d57eba6cb0
Ansonhkg <ansonox@gmail.com>
2024-01-23 02:45:18 +0300

    rm unused code

--- e2e-nodejs/group-hotwallet-with-blockhash/test-hotwallet-with-blockhash.mjs
+++ e2e-nodejs/group-hotwallet-with-blockhash/test-hotwallet-with-blockhash.mjs
@@ -119,13 +119,6 @@ export async function main() {
 
   console.log(sessionSigs);
 
-  // console.log(authSig);
-
-  // if (authSig.signedMessage.includes(TEST_BLOCKHASH)) {
-  //   // return fail("authSig doesn't contain the blockhash");
-  //   console.log("authSig doesn't contain the blockhash");
-  // }
-
   if (!checkNonceInEachSessionSig(sessionSigs, nonce)) {
     return fail("sessionSig doesn't contain the blockhash");
   }
