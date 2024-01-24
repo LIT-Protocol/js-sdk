468c5b52e4fff9168578c52a08101f460a2ca9bb
Ansonhkg <ansonox@gmail.com>
2024-01-24 15:09:28 +0300

    add instru

--- e2e-nodejs/index.mjs
+++ e2e-nodejs/index.mjs
@@ -58,6 +58,7 @@ async function main() {
      CHECK_SEV=true (enable sev attestation checks)
      MINT_NEW=true (mint new pkp resources for test run)
      REAL_TX=true yarn (Enables real tx that costs gas)
+     USE_CACHE=true (Use cached config)
 
   🚩 Flags:
       --filter=<keyword> (Filters files by keyword)
