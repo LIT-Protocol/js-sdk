a7395389d40d46dbe5bc0f73cd59164e95710b50
Josh Long <josh@litprotocol.com>
2024-01-26 15:07:33 -0500

    gen docs

--- packages/contracts-sdk/src/lib/auth-utils.spec.ts
+++ packages/contracts-sdk/src/lib/auth-utils.spec.ts
@@ -1,7 +1,7 @@
 import { getEthAuthMethodId } from './auth-utils';
 //TODO: update with tests of hashing implementation for auth method ids.
 describe('auth-utils', () => {
-    it('place holder', () => {
-        expect(1).toEqual(1);
-    });
+  it('place holder', () => {
+    expect(1).toEqual(1);
+  });
 });
