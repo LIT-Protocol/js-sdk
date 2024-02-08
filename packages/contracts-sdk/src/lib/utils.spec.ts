adf2f5c82052d0797c8a1f1dc5de456315deca17
Anson <ansonox@gmail.com>
2024-02-08 18:58:54 +0300

    Feature/lit 2413 js sdk re introduce request per second when minting rli (#345)
    * feat: different context when minting rli
    
    * feat: add context on minting rli
    
    * chore: update test cases
    
    * feat: add utils for conversions
    
    * fix: round up
    
    * fix: conversion
    
    * fix: tests
    
    * chore: update scripts
    
    * fix: stricter check
    
    * fix: stricter check
    
    * fix: stricter check
    
    * fix: add diff types
    
    * prettified

---
+++ packages/contracts-sdk/src/lib/utils.spec.ts
@@ -0,0 +1,37 @@
+import { requestsToDay, requestsToKilosecond, requestsToSecond } from './utils';
+
+describe('conversion', () => {
+  describe('requestsToKilosecond', () => {
+    test('converts per day to per kilosecond correctly', () => {
+      expect(requestsToKilosecond({ requests: 86, period: 'day' })).toBe(1);
+    });
+
+    test('converts per second to per kilosecond correctly', () => {
+      expect(requestsToKilosecond({ requests: 1, period: 'second' })).toBe(
+        1000
+      );
+    });
+  });
+
+  describe('requestsToDay', () => {
+    test('converts requests per second to requests per day correctly', () => {
+      expect(requestsToDay({ requests: 1, period: 'second' })).toBe(86400);
+    });
+
+    test('converts requests per kilosecond to requests per day correctly', () => {
+      expect(requestsToDay({ requests: 1, period: 'kilosecond' })).toBe(86);
+    });
+  });
+
+  describe('requestsToSecond', () => {
+    test('converts requests per day to requests per second correctly', () => {
+      expect(requestsToSecond({ requests: 86400, period: 'day' })).toBe(1);
+    });
+
+    test('converts requests per kilosecond to requests per second correctly', () => {
+      expect(requestsToSecond({ requests: 1, period: 'kilosecond' })).toBe(
+        1000
+      );
+    });
+  });
+});
