ef5a3cbe85c9110afe1ca05802bfb839f3c816b4
Chris Cassano <chris@litprotocol.com>
2024-03-05 03:19:43 -0700

    fix: remove .yalc folder

--- .yalc/@lit-protocol/lit-node-client-nodejs/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    var LitNodeClientNodeJs: any;
-}
-export * from './lib/lit-node-client-nodejs';
-export { decryptToFile, decryptToString, decryptToZip, decryptZipFileWithMetadata, decryptFromIpfs, encryptFile, encryptFileAndZipWithMetadata, encryptString, encryptToIpfs, encryptZip, verifyJwt, zipAndEncryptFiles, zipAndEncryptString, } from '@lit-protocol/encryption';
-export { hashResourceIdForSigning, humanizeAccessControlConditions, } from '@lit-protocol/access-control-conditions';
-export { base64StringToBlob, blobToBase64String, } from '@lit-protocol/misc-browser';
-export { uint8arrayFromString, uint8arrayToString, } from '@lit-protocol/uint8arrays';
