06be894251800082c309020c46ba436230bcb9d3
Ansonhkg <ansonox@gmail.com>
2024-04-05 23:37:40 +0100

    feat(test): update test script to accept flags

---
+++ e2e-nodejs/bun-tests/setup/utils.ts
@@ -0,0 +1,52 @@
+import { ENV } from "./env-setup";
+
+export const getNetworkFlag = (): ENV => {
+  const networkArg = process.argv.find((arg) => arg.startsWith('--network='));
+
+  const network: string = networkArg ? networkArg.replace('--network=', '') : 'localhost';
+
+  if (network !== ENV.LOCALHOST && network !== ENV.HABANERO && network !== ENV.MANZANO) {
+    console.log('Invalid network argument. Please use --network=localhost, --network=habanero, or --network=manzano');
+    process.exit();
+  }
+
+  return network;
+}
+
+// Function to parse command line arguments for filters
+export const getFiltersFlag = (): string[] => {
+  const filterArg = process.argv.find((arg) => arg.startsWith('--filter='));
+  return filterArg ? filterArg.replace('--filter=', '').split(',') : [];
+};
+
+// get --show flag
+export const showTests = (tests): void => {
+  const testsArg = process.argv.find((arg) => arg.startsWith('--show'));
+
+  const showTests = testsArg === undefined ? false : true;
+
+  if (showTests) {
+    console.log('Available tests:');
+    console.log(`Run with --filter=testName to run a specific test`);
+    console.log('----------------');
+    Object.entries(tests).forEach(([testName, testFunction], i) => {
+      console.log(`${i + 1}. ${testName}`);
+    });
+    console.log('\n')
+    process.exit();
+  }
+};
+
+export const runTests = async (tests) => {
+  const filters = getFiltersFlag();
+  const testsToRun = Object.entries(tests).filter(
+    ([testName]) => filters.length === 0 || filters.includes(testName)
+  );
+
+  for (const [testName, testFunction] of testsToRun) {
+    console.log(`Running ${testName}...`);
+
+    // @ts-ignore
+    await testFunction();
+  }
+};
--- e2e-nodejs/bun-tests/test.ts
+++ e2e-nodejs/bun-tests/test.ts
@@ -6,24 +6,12 @@ import {
   AuthSigCallbackType,
   getAuthSigCallback,
 } from '@lit-protocol/auth-helpers';
-import { ENV, devEnv } from './setup/env-setup';
+import { devEnv } from './setup/env-setup';
 import * as ethers from 'ethers';
-import { SiweMessage } from 'siwe';
-import { SessionSigs } from '@lit-protocol/types';
+import { getNetworkFlag, showTests, runTests } from './setup/utils';
 
-const {
-  litNodeClient,
-  litContractsClient,
-  hotWallet,
-  hotWalletAuthSig,
-  hotWalletAuthMethod,
-  hotWalletOwnedPkp,
-  hotWalletAuthMethodOwnedPkp,
-  lastestBlockhash,
-  capacityTokenId,
-  capacityDelegationAuthSig,
-} = await devEnv({
-  env: ENV.HABANERO,
+const devEnvPromise = devEnv({
+  env: getNetworkFlag(),
   debug: true,
 });
 
@@ -109,72 +97,21 @@ const tests = {
   },
 };
 
-// Function to parse command line arguments for filters
-const getFilters = (): string[] => {
-  const filterArg = process.argv.find((arg) => arg.startsWith('--filter='));
-  return filterArg ? filterArg.replace('--filter=', '').split(',') : [];
-};
+showTests(tests);
 
-// Function to run tests based on filters
-const runTests = async () => {
-  const filters = getFilters();
-  const testsToRun = Object.entries(tests).filter(
-    ([testName]) => filters.length === 0 || filters.includes(testName)
-  );
+const {
+  litNodeClient,
+  litContractsClient,
+  hotWallet,
+  hotWalletAuthSig,
+  hotWalletAuthMethod,
+  hotWalletOwnedPkp,
+  hotWalletAuthMethodOwnedPkp,
+  lastestBlockhash,
+  capacityTokenId,
+  capacityDelegationAuthSig,
+} = await devEnvPromise;
 
-  for (const [testName, testFunction] of testsToRun) {
-    console.log(`Running ${testName}...`);
-    await testFunction();
-  }
-};
-
-await runTests();
-
-// process.exit();
-
-// const authNeededCallback = async (params) => {
-
-//   console.log("params:", params);
-
-//   // write the params to local file
-//   const fs = require('fs');
-//   fs.writeFileSync('./e2e-nodejs/bun-tests/logs/auth-needed-callback-params.json', JSON.stringify(params, null, 2));
-
-//   const response = await litNodeClient.signSessionKey({
-//     // sessionKey: params.sessionKeyPair,
-//     sessionKeyUri: params.sessionKeyUri,
-//     statement: params.statement,
-//     authMethods: [hotWalletAuthMethod],
-//     pkpPublicKey: hotWalletAuthMethodOwnedPkp.publicKey,
-//     expiration: params.expiration,
-//     resources: params.resources,
-//     chainId: 1,
-//     resourceAbilityRequests: resourceAbilityRequests,
-//   });
-//   return response.authSig;
-// };
-
-// const sessionSigs = await litNodeClient.getSessionSigs({
-//   pkpPublicKey: hotWalletAuthMethodOwnedPkp.publicKey,
-//   chain: 'ethereum',
-//   resourceAbilityRequests,
-//   authNeededCallback: authNeededCallback,
-//   capacityDelegationAuthSig,
-// });
-
-// console.log('sessionSigs:', sessionSigs);
-
-// // -- execute js
-// const TO_SIGN = ethers.utils.arrayify(ethers.utils.keccak256([1, 2, 3, 4, 5]));
-
-// const res = await litNodeClient.executeJs({
-//   sessionSigs,
-//   code: `(async() => {
-//     console.log("Testing!");
-//   })()`,
-//   jsParams: {},
-// });
-
-// console.log('res:', res);
+await runTests(tests);
 
 process.exit();
