b55929a623f26514c7a896648cc41ad29616166d
Ansonhkg <ansonox@gmail.com>
2024-04-11 22:34:36 +0100

    fix: getSessionSigs should accept pkpPublicKey as a param

--- packages/types/src/lib/interfaces.ts
+++ packages/types/src/lib/interfaces.ts
@@ -969,6 +969,8 @@ export interface GetSignSessionKeySharesProp {
 }
 
 export interface GetSessionSigsProps {
+  pkpPublicKey?: string;
+
   // When this session signature will expire.  The user will have to reauthenticate after this time using whatever auth method you set up.  This means you will have to call this signSessionKey function again to get a new session signature.  This is a RFC3339 timestamp.  The default is 24 hours from now.
   expiration?: any;
 
