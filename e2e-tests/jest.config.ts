53ffb2c13f78379e1c27d599485539ab07ed3661
Ansonhkg <ansonox@gmail.com>
2024-04-16 04:29:17 +0100

    feat(test): setup jest testing environment

---
+++ e2e-tests/jest.config.ts
@@ -0,0 +1,11 @@
+// import { getJestProjects } from '@nx/jest';
+
+export default {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // maxWorkers: 1,
+};
---
+++ e2e-tests/setup/env-setup.ts
@@ -0,0 +1,64 @@
+import { log } from '@lit-protocol/misc';
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import * as networkContextData from './networkContext.json';
+
+let litNetwork = process.env.NETWORK as any;
+const debug = process.env.DEBUG === 'true' ? true : false;
+
+export type TESTABLE_NETWORK_TYPE =
+  | 'habanero'
+  | 'manzano'
+  | 'cayenne'
+  | 'custom';
+
+export interface DevEnv {
+  network: TESTABLE_NETWORK_TYPE;
+  litNodeClient: LitNodeClient;
+}
+
+export const devEnv = async (params?: {
+  network?: TESTABLE_NETWORK_TYPE;
+}): Promise<DevEnv> => {
+  /**
+   * ====================================
+   * Setting up Lit Node Client
+   * ====================================
+   */
+  log('🧪 [env-setup.ts] Starting devEnv');
+  let litNodeClient: LitNodeClient;
+
+  litNetwork = params?.network || litNetwork;
+
+  if (litNetwork === 'custom') {
+    litNodeClient = new LitNodeClient({
+      litNetwork: 'custom',
+      bootstrapUrls: [
+        'http://127.0.0.1:7470',
+        'http://127.0.0.1:7471',
+        'http://127.0.0.1:7472',
+      ],
+      debug,
+      checkNodeAttestation: false,
+      networkContext: networkContextData,
+    });
+  } else if (litNetwork === 'manzano' || litNetwork === 'habanero') {
+    litNodeClient = new LitNodeClient({
+      litNetwork,
+      debug,
+      checkNodeAttestation: true,
+    });
+  } else {
+    litNodeClient = new LitNodeClient({
+      litNetwork: 'cayenne',
+      debug,
+      checkNodeAttestation: false,
+    });
+  }
+
+  await litNodeClient.connect();
+
+  return {
+    network: litNetwork,
+    litNodeClient,
+  };
+};
