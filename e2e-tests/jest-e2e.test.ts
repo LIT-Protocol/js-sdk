80632c25ed359164fb6c39004f25242ec7121f85
Ansonhkg <ansonox@gmail.com>
2024-04-16 04:29:17 +0100

    feat(test): setup jest testing environment

---
+++ e2e-tests/jest-e2e.test.ts
@@ -0,0 +1,23 @@
+// Test command
+// yarn jest:e2e
+
+import { DevEnv, TESTABLE_NETWORK_TYPE, devEnv } from './setup/env-setup';
+
+try {
+  jest.setTimeout(60000);
+} catch (e) {
+  // ... continue execution
+}
+
+describe('connections', () => {
+  test('test multiple networks', async () => {
+    for (let network of ['habanero', 'manzano', 'cayenne']) {
+      console.log(`Testing network: ${network}`);
+      const litDev: DevEnv = await devEnv({
+        network: network as TESTABLE_NETWORK_TYPE,
+      });
+      expect(litDev.litNodeClient.ready).toBe(true);
+      expect(litDev.litNodeClient.config.litNetwork).toBe(network);
+    }
+  });
+});
