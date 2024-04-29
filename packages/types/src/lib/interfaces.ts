415da36d70597bc8e06bdd6af513044e119f6f28
Anson <ansonox@gmail.com>
2024-04-29 19:06:43 +0100

    fix: pass sessionKey to getSessionSigs

--- packages/types/src/lib/interfaces.ts
+++ packages/types/src/lib/interfaces.ts
@@ -945,7 +945,7 @@ export interface GetSessionSigsProps {
    * The serialized session key pair to sign.
    * If not provided, a session key pair will be fetched from localStorge or generated.
    */
-  sessionKey?: any;
+  sessionKey?: SessionKeyPair;
 
   // rateLimitAuthSig: AuthSig;
 
