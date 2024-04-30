3b7de4561cf00f2ec009ad49232a571b992a3d75
Anson <ansonox@gmail.com>
2024-04-30 16:33:35 +0100

    fix: https://github.com/LIT-Protocol/js-sdk/pull/440#discussion_r1583803745

--- packages/lit-node-client-nodejs/src/lib/lit-node-client-nodejs.ts
+++ packages/lit-node-client-nodejs/src/lib/lit-node-client-nodejs.ts
@@ -173,6 +173,11 @@ export class LitNodeClientNodeJs
   createCapacityDelegationAuthSig = async (
     params: CapacityCreditsReq
   ): Promise<CapacityCreditsRes> => {
+    // -- validate
+    if (!params.dAppOwnerWallet) {
+      throw new Error('dAppOwnerWallet must exist');
+    }
+
     // Useful log for debugging
     if (!params.delegateeAddresses || params.delegateeAddresses.length === 0) {
       log(
@@ -189,11 +194,6 @@ export class LitNodeClientNodeJs
     // -- if it's not ready yet, then connect
     if (!this.ready) {
       await this.connect();
-    }
-
-    // -- validate
-    if (!params.dAppOwnerWallet) {
-      throw new Error('dAppOwnerWallet must exist');
     }
 
     const nonce = await this.getLatestBlockhash();
