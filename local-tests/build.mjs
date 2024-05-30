a6b3e7b526683b840079658909e44b4c0a99addd
Anson <ansonox@gmail.com>
2024-05-30 12:57:48 +0100

    feat(tinny): add `package.json` generation

--- local-tests/build.mjs
+++ local-tests/build.mjs
@@ -55,10 +55,46 @@ try {
   }
 };
 
+/**
+ * Copies the package.json file to the build directory and adds the root package.json dependencies to the build package.json.
+ * @throws {Error} If there is an error in copying the package.json file or updating the dependencies.
+ */
+export const postBuildMoveAndSetupPackageJsonToBuildDir = () => {
+  const ROOT_PACKAGEJSON = './package.json';
+  const COPY_FROM = `./${TEST_DIR}/package.temp.json`;
+  const COPY_TO = `./${TEST_DIR}/build/package.json`;
+
+  // -- Copy the package.json file to the build directory
+  try {
+    fs.copyFileSync(COPY_FROM, COPY_TO);
+  } catch (e) {
+    throw new Error(
+      `Error in postBuildMoveAndSetupPackageJsonToBuildDir: ${e}`
+    );
+  }
+
+  // -- Copy the root package.json dependencies to the build package.json
+  try {
+    const rootPackageJson = JSON.parse(fs.readFileSync(ROOT_PACKAGEJSON));
+    const buildPackageJson = JSON.parse(fs.readFileSync(COPY_TO));
+
+    // Add the dependencies to the build package.json
+    buildPackageJson.dependencies = rootPackageJson.dependencies;
+
+    // Write the updated package.json
+    fs.writeFileSync(COPY_TO, JSON.stringify(buildPackageJson, null, 2));
+  } catch (e) {
+    throw new Error(
+      `Error in postBuildMoveAndSetupPackageJsonToBuildDir: ${e}`
+    );
+  }
+};
+
 // Go!
 (async () => {
   const start = Date.now();
   await build();
   postBuildPolyfill();
+  postBuildMoveAndSetupPackageJsonToBuildDir();
   console.log(`[build.mjs] 🚀 Build time: ${Date.now() - start}ms`);
 })();
