711820cf99887076aa45fef9c64dfcf3649a1a39
Josh Long <josh@litprotocol.com>
2024-06-06 12:08:54 -0400

    ref(tinny): add stack trace printing on error.

--- local-tests/setup/tinny-operations.ts
+++ local-tests/setup/tinny-operations.ts
@@ -189,7 +189,7 @@ export const runTestsParallel = async ({
               }. ${testName} - Failed after ${maxAttempts} attempts (${timeTaken} ms)\x1b[0m`
             );
             console.error(`\x1b[31m❌Error:\x1b[90m ${error}\x1b[0m`);
-            return `${testName} (Failed in ${timeTaken} ms) - Error: ${error}`;
+            return `${testName} (Failed in ${timeTaken} ms) - Error: ${error} \n Stack Trace: ${error.stack}`;
           }
         }
       }
