d85753518b92c881ff16d714577fcd67bba3e752
Wyatt Barnes <me@wyatt.email>
2024-06-10 21:15:19 -1000

    Debugging dist file path resolution

--- packages/wrapped-keys/src/lib/litActions/solana/signMessageWithSolanaEncryptedKey.ts
+++ packages/wrapped-keys/src/lib/litActions/solana/signMessageWithSolanaEncryptedKey.ts
@@ -1,9 +1,12 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
-const bundledFilePath = './dist/signMessageWithSolanaEncryptedKey.js';
+const bundledFilePath = resolve(
+  __dirname,
+  './dist/signMessageWithSolanaEncryptedKey.js'
+);
 
 export const signMessageWithSolanaEncryptedKeyLitAction = readFileSync(
-  resolve(bundledFilePath),
+  bundledFilePath,
   'utf-8'
 );
