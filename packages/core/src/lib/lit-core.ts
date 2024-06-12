cee8344884220da09fbfab03cfbc2a244ca0487b
Josh Long <josh@litprotocol.com>
2024-06-12 10:58:47 -0400

    fix(core): contract context

--- packages/core/src/lib/lit-core.ts
+++ packages/core/src/lib/lit-core.ts
@@ -491,7 +491,11 @@ export class LitCore {
           this.config.rpcUrl || LIT_CHAINS['lit'].rpcUrls[0]
         )
       );
-    } else if (!this.config.contractContext.Staking) {
+    } else if (
+      !this.config.contractContext.Staking &&
+      !this.config.contractContext.resolverAddress
+    ) {
+      console.log(this.config.contractContext);
       throw new Error(
         'The provided contractContext was missing the "Staking" contract`'
       );
