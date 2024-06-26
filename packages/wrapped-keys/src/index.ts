5b5abb14c8e909e33d3dc9275810dcb2b0edbd30
Daryl Collins <daryl@litprotocol.com>
2024-06-26 01:05:59 +0100

    chore(wrapped-keys): LIT-3125 - Update entrypoint exports to be explicit. - Namespaced API methods and constants separately from exported types - Only exported types, interfaces and constants that are part of the API layer that end consumers will interact with

--- packages/wrapped-keys/src/index.ts
+++ packages/wrapped-keys/src/index.ts
@@ -1,3 +1,60 @@
-export * from './lib/wrapped-keys';
-export * from './lib/interfaces';
-export { NETWORK_EVM, NETWORK_SOLANA } from './lib/constants';
+import {
+  signMessageWithEncryptedKey,
+  getEncryptedKeyMetadata,
+  exportPrivateKey,
+  generatePrivateKey,
+  importPrivateKey,
+  signTransactionWithEncryptedKey,
+} from './lib/api';
+import {
+  NETWORK_SOLANA,
+  NETWORK_EVM,
+  NETWORK_CUSTOM,
+  CHAIN_ETHEREUM,
+  LIT_PREFIX,
+} from './lib/constants';
+
+import type {
+  StoredKeyMetadata,
+  SupportedNetworks,
+} from './lib/service-client';
+import type {
+  SignMessageWithEncryptedKeyParams,
+  GetEncryptedKeyMetadataParams,
+  ExportPrivateKeyParams,
+  GeneratePrivateKeyParams,
+  ImportPrivateKeyParams,
+  SignTransactionWithEncryptedKeyParams,
+  ExportPrivateKeyResult,
+  GeneratePrivateKeyResult,
+} from './lib/types';
+
+export const constants = {
+  NETWORK_SOLANA,
+  NETWORK_EVM,
+  NETWORK_CUSTOM,
+  CHAIN_ETHEREUM,
+  LIT_PREFIX,
+};
+
+export const api = {
+  signMessageWithEncryptedKey,
+  getEncryptedKeyMetadata,
+  exportPrivateKey,
+  generatePrivateKey,
+  importPrivateKey,
+  signTransactionWithEncryptedKey,
+};
+
+export {
+  StoredKeyMetadata,
+  SupportedNetworks,
+  SignMessageWithEncryptedKeyParams,
+  GetEncryptedKeyMetadataParams,
+  ExportPrivateKeyParams,
+  ExportPrivateKeyResult,
+  GeneratePrivateKeyParams,
+  GeneratePrivateKeyResult,
+  ImportPrivateKeyParams,
+  SignTransactionWithEncryptedKeyParams,
+};
