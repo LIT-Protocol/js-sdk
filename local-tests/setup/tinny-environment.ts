7c97dfd2eb36c777d95dc23c7ae3e126d826018e
Josh Long <josh@litprotocol.com>
2024-07-26 14:46:50 -0400

    add back dotenvx

--- local-tests/setup/tinny-environment.ts
+++ local-tests/setup/tinny-environment.ts
@@ -19,8 +19,7 @@ import { ethers } from 'ethers';
 import { createSiweMessage, generateAuthSig } from '@lit-protocol/auth-helpers';
 import { ShivaClient, TestnetClient } from './shiva-client';
 
-console.log('Loading env vars from dot config...');
-console.log('Done loading env', process.env['DEBUG']);
+console.log('checking env', process.env['DEBUG']);
 export class TinnyEnvironment {
   public network: LIT_TESTNET;
 
