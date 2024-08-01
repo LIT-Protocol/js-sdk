4d93aa64dda5d918a26391737ecf24b2f92e21fe
FedericoAmura <federicoamura@gmail.com>
2024-08-01 15:50:13 +0200

    chore: linting

--- packages/contracts-sdk/src/lib/contracts-sdk.ts
+++ packages/contracts-sdk/src/lib/contracts-sdk.ts
@@ -105,7 +105,9 @@ declare global {
 
 // Due to the usage of arbitrum stylus contracts the gas limit is increased by 10% to avoid reverts due to out of gas errors
 const GAS_LIMIT_INCREASE_PERCENTAGE = 10;
-const GAS_LIMIT_ADJUSTMENT = ethers.BigNumber.from(100).add(GAS_LIMIT_INCREASE_PERCENTAGE);
+const GAS_LIMIT_ADJUSTMENT = ethers.BigNumber.from(100).add(
+  GAS_LIMIT_INCREASE_PERCENTAGE
+);
 
 // This code defines a LitContracts class that acts as a container for a collection of smart contracts. The class has a constructor that accepts an optional args object with provider and rpc properties. If no provider is specified, the class will create a default provider using the specified rpc URL. If no rpc URL is specified, the class will use a default URL.
 // The class has a number of properties that represent the smart contract instances, such as accessControlConditionsContract, litTokenContract, pkpNftContract, etc. These smart contract instances are created by passing the contract address, ABI, and provider to the ethers.Contract constructor.
