ceee540a6f9cce0a732c6e8ea5cbf727f70b24bb
Josh Long <josh@litprotocol.com>
2024-08-23 13:55:12 -0400

    chore: fmt

--- packages/core/src/lib/lit-core.ts
+++ packages/core/src/lib/lit-core.ts
@@ -930,7 +930,7 @@ export class LitCore {
     data = { ...data, epoch: this.currentEpochNumber };
 
     // If there is a `sessionSigs' object in the params remove before sending the request;
-    // this line has been added as a catch all to prevent sending with the request 
+    // this line has been added as a catch all to prevent sending with the request
     if (data.sessionSigs) {
       delete data.sessionSigs;
     }
