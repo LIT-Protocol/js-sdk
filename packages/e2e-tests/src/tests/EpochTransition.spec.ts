76c143cbb0df77ab8d84184cbee68502656292ed
Josh Long <josh@litprotocol.com>
2024-08-29 13:22:29 -0400

    test: remove await

--- packages/e2e-tests/src/tests/EpochTransition.spec.ts
+++ packages/e2e-tests/src/tests/EpochTransition.spec.ts
@@ -2,6 +2,7 @@ import { expect, jest } from '@jest/globals';
 
 import { TinnyEnvironment } from '@lit-protocol/tinny';
 
+
 try {
   jest.setTimeout(60_000 * 1_000);
 } catch (e) {
@@ -25,13 +26,13 @@ describe('Epoch Transition', () => {
   });
 
   it('Should transition epoch triggering contract event', async () => {
-    await devEnv.testnet?.transitionEpochAndWait();
-
+   devEnv.testnet?.transitionEpochAndWait().then(async () => {
     await new Promise((res) => {
-      setTimeout(res, 1_000);
+        setTimeout(res, 1_000);
+      });
+      expect(
+        devEnv.litNodeClient?.['_handleStakingContractStateChange']
+      ).toBeCalled();
     });
-    expect(
-      devEnv.litNodeClient?.['_handleStakingContractStateChange']
-    ).toBeCalled();
-  });
+   });
 });
