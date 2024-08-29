d53f8554edc327c58fe67e50e8c8421c6672ee3e
Josh Long <josh@litprotocol.com>
2024-08-29 11:35:27 -0400

    test: add initial epoch transition test

---
+++ packages/e2e-tests/src/tests/EpochTransition.spec.ts
@@ -0,0 +1,37 @@
+import { expect, jest } from '@jest/globals';
+
+import { TinnyEnvironment } from '@lit-protocol/tinny';
+
+try {
+  jest.setTimeout(60000);
+} catch (e) {
+  // ... continue execution
+}
+
+describe('Epoch Transition', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+
+  let devEnv: TinnyEnvironment;
+  beforeAll(() => {
+    //@ts-expect-error defined in global
+    devEnv = global.devEnv;
+  });
+
+  afterAll(async () => {
+    await devEnv.litNodeClient?.disconnect();
+  });
+
+  it('Should transition epoch triggering contract event', async () => {
+    await devEnv.testnet?.transitionEpochAndWait();
+
+    await new Promise((res) => {
+      setTimeout(res, 1_000);
+    });
+    expect(
+      devEnv.litNodeClient?.['_handleStakingContractStateChange']
+    ).toBeCalled();
+  });
+});
