3e5d8f6d5ceebc29886b363b0c89899ae753dc9f
Josh Long <josh@litprotocol.com>
2024-09-03 12:03:23 -0400

    test: remove unused import

--- packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
+++ packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
@@ -1,7 +1,4 @@
-import * as assert from 'node:assert';
-
 import { TinnyEnvironment } from '@lit-protocol/tinny';
-
 
 export async function testTransitionEpochShouldTriggerStakingEvent(devEnv: TinnyEnvironment) {
   await devEnv.testnet?.transitionEpochAndWait()
