d8e5491335b7d6832ad74420830ce00d07b91751
Josh Long <josh@litprotocol.com>
2024-09-03 13:14:02 -0400

    test: add further basic epoch transition assertions.

--- packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
+++ packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
@@ -1,14 +1,30 @@
 import { TinnyEnvironment } from '@lit-protocol/tinny';
 
 export async function testTransitionEpochShouldTriggerStakingEvent(devEnv: TinnyEnvironment) {
+
+  const connectedNodes: string[] | undefined = devEnv.litNodeClient?.config.bootstrapUrls;
   await devEnv.testnet?.transitionEpochAndWait()
 
   await new Promise((res) => {
     setTimeout(res, 1_000);
   });
   
-  if (! devEnv.litNodeClient?.ready) {
+  
+  const currentEpoch = devEnv.litNodeClient?.currentEpochNumber;
+
+  if (!devEnv.litNodeClient?.ready) {
     throw new Error("Nodes not connected after epoch transition");
+  }
+
+  if (devEnv.litNodeClient?.config.bootstrapUrls !== connectedNodes) {
+    throw new Error("Connected nodes does not match original node set pre epoch transiton");
+  }
+
+
+  if (currentEpoch as number + 1 != devEnv.litNodeClient.currentEpochNumber) {
+    console.log(currentEpoch);
+    console.log(devEnv.litNodeClient?.currentEpochNumber);
+    throw new Error('Epoch value is not what was expected');
   }
 }
 
