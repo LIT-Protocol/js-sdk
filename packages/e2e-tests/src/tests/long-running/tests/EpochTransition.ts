2cce825470dd5f51a7a40e63acaff3eb7c9d471b
Josh Long <josh@litprotocol.com>
2024-09-03 09:27:34 -0400

    ref: test assert

--- packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
+++ packages/e2e-tests/src/tests/long-running/tests/EpochTransition.ts
@@ -1,13 +1,17 @@
+import * as assert from 'node:assert';
+
 import { TinnyEnvironment } from '@lit-protocol/tinny';
+
 
 export async function testTransitionEpochShouldTriggerStakingEvent(devEnv: TinnyEnvironment) {
   await devEnv.testnet?.transitionEpochAndWait().then(async () => {
     await new Promise((res) => {
         setTimeout(res, 1_000);
       });
-      expect(
-        devEnv.litNodeClient?.['_handleStakingContractStateChange']
-      ).toBeCalled();
+      
+      assert.ok(
+        devEnv.litNodeClient?.ready
+      );
     });
 }
 
