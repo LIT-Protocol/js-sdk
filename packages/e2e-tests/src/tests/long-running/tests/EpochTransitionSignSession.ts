d8a259e58096582036d9b6867e5b60cb61678c71
Josh Long <josh@litprotocol.com>
2024-09-06 11:36:39 -0400

    dev: add handshake test during epoch transition

--- packages/e2e-tests/src/tests/long-running/tests/EpochTransitionSignSession.ts
+++ packages/e2e-tests/src/tests/long-running/tests/EpochTransitionSignSession.ts
@@ -1,43 +1,65 @@
-import { LitAbility, LitActionResource, LitPKPResource } from '@lit-protocol/auth-helpers';
+import {
+  LitAbility,
+  LitActionResource,
+  LitPKPResource,
+} from '@lit-protocol/auth-helpers';
 import { TinnyEnvironment, getPkpSessionSigs } from '@lit-protocol/tinny';
 
-export async function epochTransitionSignSessioShouldValidateAfterTransition(devEnv: TinnyEnvironment) {
-    const alice = await devEnv.createRandomPerson();
-    await devEnv.testnet?.transitionEpochAndWait();
+export async function testEpochTransitionSignSessioShouldValidateAfterTransition(
+  devEnv: TinnyEnvironment
+) {
+  const alice = await devEnv.createRandomPerson();
+  await devEnv.testnet?.transitionEpochAndWait();
 
-    const session = await getPkpSessionSigs(devEnv, alice, [
-        {
-          resource: new LitPKPResource('*'),
-          ability: LitAbility.PKPSigning,
-        },
-        {
-          resource: new LitActionResource('*'),
-          ability: LitAbility.LitActionExecution,
-        },
-    ]);
+  const session = await getPkpSessionSigs(devEnv, alice, [
+    {
+      resource: new LitPKPResource('*'),
+      ability: LitAbility.PKPSigning,
+    },
+    {
+      resource: new LitActionResource('*'),
+      ability: LitAbility.LitActionExecution,
+    },
+  ]);
 
-    if (!session) {
-        throw new Error('Could not sign session');
-    }
+  if (!session) {
+    throw new Error('Could not sign session');
+  }
 }
 
+export async function testEpochTransitionSignSessioShouldValidateDuringTransition(
+  devEnv: TinnyEnvironment
+) {
+  const alice = await devEnv.createRandomPerson();
+  devEnv.testnet?.transitionEpochAndWait();
+  // give some time for the transition to start
+  await new Promise<void>((res) => {
+    setTimeout(res, 5_000);
+  });
+  const session = await getPkpSessionSigs(devEnv, alice, [
+    {
+      resource: new LitPKPResource('*'),
+      ability: LitAbility.PKPSigning,
+    },
+    {
+      resource: new LitActionResource('*'),
+      ability: LitAbility.LitActionExecution,
+    },
+  ]);
 
-export async function epochTransitionSignSessioShouldValidateDuringTransition(devEnv: TinnyEnvironment) {
-    const alice = await devEnv.createRandomPerson();
-    devEnv.testnet?.transitionEpochAndWait();
+  if (!session) {
+    throw new Error('Could not sign session');
+  }
+}
 
-    const session = await getPkpSessionSigs(devEnv, alice, [
-        {
-          resource: new LitPKPResource('*'),
-          ability: LitAbility.PKPSigning,
-        },
-        {
-          resource: new LitActionResource('*'),
-          ability: LitAbility.LitActionExecution,
-        },
-    ]);
+export async function testEpochTransitionHandshakeShouldReturnUniformEpoch(
+  devEnv: TinnyEnvironment
+) {
+  devEnv.testnet?.transitionEpochAndWait();
+  // give some time for the transition to start
+  await new Promise<void>((res) => {
+    setTimeout(res, 5_000);
+  });
 
-    if (!session) {
-        throw new Error('Could not sign session');
-    }
+  await devEnv.litNodeClient?.connect();
 }
