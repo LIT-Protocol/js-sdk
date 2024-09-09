2db5012f595753cafe148351df1396178da48210
Josh Long <josh@litprotocol.com>
2024-09-09 14:47:54 -0400

    ref: test global hooks in jest

--- packages/e2e-tests/jest.config.ts
+++ packages/e2e-tests/jest.config.ts
@@ -1,5 +1,5 @@
-/* eslint-disable */
-export default {
+/** @type {import('jest').Config} */
+const config = {
   displayName: 'e2e-tests',
   preset: 'ts-jest',
   transform: {
@@ -9,4 +9,9 @@ export default {
   coverageDirectory: '../../coverage/packages/e2e-tests',
   setupFilesAfterEnv: ['../../jest.setup.js'],
   testEnvironment: './setup.config.js',
+  globalSetup: './setup.jest.js',
+  globalTeardown: './teardown.jest.js'
 };
+
+
+module.exports = config;
---
+++ packages/e2e-tests/setup.jest.js
@@ -0,0 +1,5 @@
+module.exports = async function (globalConfig, projectConfig) {
+    console.log('test global setup fires once')
+    console.log(globalConfig.testPathPattern);
+    console.log(projectConfig.cache);  
+};
