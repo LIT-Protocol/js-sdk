9803e36e0e7325e4d5b54c3964f382b749ae87fc
Josh Long <josh@litprotocol.com>
2024-09-11 13:33:47 -0400

    ref: fix pathing in ci jest config

--- packages/e2e-tests/ci/jest.config.ts
+++ packages/e2e-tests/ci/jest.config.ts
@@ -10,8 +10,8 @@ const config = {
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/packages/e2e-tests',
   setupFilesAfterEnv: ['../../../jest.setup.js'],
-  testEnvironment: './../../setup.config.js',
-  globalSetup: './../../setup.jest.js',
+  testEnvironment: './../setup.config.js',
+  globalSetup: './../setup.jest.js',
   globalTeardown: './../teardown.jest.js',
   testPathIgnorePatterns: [
     './../src/tests/Epoch',
