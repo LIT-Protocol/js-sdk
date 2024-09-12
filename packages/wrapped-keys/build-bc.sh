6a6c4502f89ff7d324cba2adca5c9c4da0dc6ff7
Anson <ansonox@gmail.com>
2024-09-12 23:21:05 +0100

    fix: add support for different os types for the sed shell script command

--- packages/wrapped-keys/build-bc.sh
+++ packages/wrapped-keys/build-bc.sh
@@ -8,4 +8,27 @@ set -e
 cp -r ../../dist/packages/wrapped-keys ../../dist/packages/wrapped-keys-bc
 yarn bundle
 cp -r ../wrapped-keys/src/lib/generated/litActions ../../dist/packages/wrapped-keys-bc/src/lib/generated
-sed -i 's/wrapped-keys/wrapped-keys-bc/' ../../dist/packages/wrapped-keys-bc/package.json
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  sed -i 's/wrapped-keys/wrapped-keys-bc/' ../../dist/packages/wrapped-keys-bc/package.json
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  sed -i '' 's/wrapped-keys/wrapped-keys-bc/' ../../dist/packages/wrapped-keys-bc/package.json
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  # POSIX compatibility layer and Linux environment emulation for Windows
+  sed -i 's/wrapped-keys/wrapped-keys-bc/' ../../dist/packages/wrapped-keys-bc/package.json
+elif [[ "$OSTYPE" == "msys" ]]; then
+  # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+  sed -i 's/wrapped-keys/wrapped-keys-bc/' ../../dist/packages/wrapped-keys-bc/package.json
+elif [[ "$OSTYPE" == "win32" ]]; then
+  # I'm not sure this can happen.
+  echo "win32 detected, sed might not be supported in this environment."
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+  # FreeBSD
+  echo "FreeBSD detected, sed -i might not be supported."
+else
+  # Unknown OS
+  echo "Unknown operating system: $OSTYPE. sed -i might not be supported."
+fi
+´
