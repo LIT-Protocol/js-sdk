d2eaa788572ea387930dbb93de28b1acf2ff18ce
Anson <ansonox@gmail.com>
2024-09-19 17:06:42 +0100

    feat: add `ParsedSignedMessage` interface

--- packages/types/src/lib/interfaces/session-sigs.ts
+++ packages/types/src/lib/interfaces/session-sigs.ts
@@ -1,6 +1,16 @@
 import { AuthSig } from '../interfaces';
 
 export interface ParsedSignedMessage {
+  // Known keys
+  URI?: string;
+  Version?: string;
+  'Chain ID'?: string;
+  Nonce?: string;
+  'Issued At'?: string;
+  'Expiration Time'?: string;
+  Resources?: string[]; // Should be an array of strings
+  // Dynamic keys
+
   [key: string]: any;
 }
 
