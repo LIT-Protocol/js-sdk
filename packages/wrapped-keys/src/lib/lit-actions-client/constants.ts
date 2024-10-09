8d517d8f248c173d817a0fe9ac097d6462d891fa
Daryl Collins <daryl@litprotocol.com>
2024-10-09 19:46:37 +0100

    chore(wrapped-keys-lit-actions): LIT-3920 - Update IPFS CIDs to use current lit action code from this commit

--- packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
+++ packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
@@ -2,25 +2,25 @@ import { LitCidRepository, LitCidRepositoryCommon } from './types';
 
 const LIT_ACTION_CID_REPOSITORY: LitCidRepository = Object.freeze({
   signTransaction: Object.freeze({
-    evm: 'QmRWGips9G3pHXNa3viGFpAyh1LwzrR35R4xMiG61NuHpS',
-    solana: 'QmPZR6FnTPMYpzKxNNHt4xRckDsAoQz76cxkLhJArSoe4w',
+    evm: 'QmSr9TLHUFcGMrQQrNXRsYxcgaBFtcCqWi4sgbevbhmrhv',
+    solana: 'QmS223QiJkPfncUhY2HGQmme7LRgg93NBy8tU1fgGFUUzu',
   }),
   signMessage: Object.freeze({
-    evm: 'QmNy5bHvgaN2rqo4kMU71jtgSSxDES6HSDgadBV29pfcRu',
-    solana: 'Qma1nRZN5eriT1a7Uffbiek5jsksvWCCqHuE1x1nk9zaAq',
+    evm: 'QmXi9iqJvXrHoUGSo5WREonrruDhzQ7cFr7Cry3wX2hmue',
+    solana: 'QmcEJGVqRYtVukjm2prCPT7Fs66GpaqZwmZoxEHMHor6Jz',
   }),
   generateEncryptedKey: Object.freeze({
-    evm: 'QmaoPMSqcze3NW3KSA75ecWSkcmWT1J7kVr8LyJPCKRvHd',
-    solana: 'QmdRBXYLYvcNHrChmsZ2jFDY8dA99CcSdqHo3p1ES3UThL',
+    evm: 'QmPzycC9bN4QfhVWy7ggL4u9Gr1vQz3Gx4MYh4vvpvqBzc',
+    solana: 'QmYrmzhh7ZoK8LoZ854DmfdRacmZgpVz9c3kW6u5htcdc5',
   }),
   exportPrivateKey: Object.freeze({
-    evm: 'Qmb5ZAm1EZRL7dYTtyYxkPxx4kBmoCjjzcgdrJH9cKMXxR',
-    solana: 'Qmb5ZAm1EZRL7dYTtyYxkPxx4kBmoCjjzcgdrJH9cKMXxR',
+    solana: 'QmUJ74pTUqeeHzDGdfwCph1vJVNJ1rRzJdvMiTjS1BMwYj',
+    evm: 'QmUJ74pTUqeeHzDGdfwCph1vJVNJ1rRzJdvMiTjS1BMwYj',
   }),
 });
 
 const LIT_ACTION_CID_REPOSITORY_COMMON: LitCidRepositoryCommon = Object.freeze({
-  batchGenerateEncryptedKeys: '',
+  batchGenerateEncryptedKeys: 'QmTMCnEd5M45EVSBvayc8gAvFsNYRnPjf5sfbtR44bWt68',
 });
 
 export { LIT_ACTION_CID_REPOSITORY, LIT_ACTION_CID_REPOSITORY_COMMON };
