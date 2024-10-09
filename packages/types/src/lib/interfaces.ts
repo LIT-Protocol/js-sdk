d490046b1d78a4fe5de95fd7d85b945d52d2bc8d
Anson <ansonox@gmail.com>
2024-10-09 18:18:48 +0100

    feat: add advanced controls to executeJs

--- packages/types/src/lib/interfaces.ts
+++ packages/types/src/lib/interfaces.ts
@@ -314,7 +314,7 @@ export interface JsonSignChainDataRequest {
 
 export interface JsonSignSessionKeyRequestV1
   extends Pick<LitActionSdkParams, 'jsParams'>,
-    Pick<LitActionSdkParams, 'litActionIpfsId'> {
+  Pick<LitActionSdkParams, 'litActionIpfsId'> {
   sessionKey: string;
   authMethods: AuthMethod[];
   pkpPublicKey?: string;
@@ -502,7 +502,8 @@ export interface JsonExecutionSdkParamsTargetNode
 }
 
 export interface JsonExecutionSdkParams
-  extends Pick<LitActionSdkParams, 'jsParams'> {
+  extends Pick<LitActionSdkParams, 'jsParams'>,
+  ExecuteJsAdvancedOptions {
   /**
    *  JS code to run on the nodes
    */
@@ -522,6 +523,9 @@ export interface JsonExecutionSdkParams
    * auth methods to resolve
    */
   authMethods?: AuthMethod[];
+}
+
+export interface ExecuteJsAdvancedOptions {
 
   /**
    * a strategy for proccessing `reponse` objects returned from the
@@ -529,7 +533,20 @@ export interface JsonExecutionSdkParams
    */
   responseStrategy?: LitActionResponseStrategy;
 
+  /**
+   * Allow overriding the default `code` property in the `JsonExecutionSdkParams`
+   */
   ipfsOptions?: IpfsOptions;
+
+  /**
+   * number of responses required to consider the execution successful
+   */
+  numResponsesRequired?: number;
+
+  /**
+   * idea: the number of nodes to pay for running executions
+   */
+  // numNodesToRunOn?: number;
 }
 
 export interface JsonExecutionRequestTargetNode extends JsonExecutionRequest {
@@ -568,7 +585,7 @@ export interface SessionSigsOrAuthSig {
 
 export interface DecryptRequestBase
   extends SessionSigsOrAuthSig,
-    MultipleAccessControlConditions {
+  MultipleAccessControlConditions {
   /**
    * The chain name of the chain that this contract is deployed on.  See LIT_CHAINS for currently supported chains.
    */
@@ -613,7 +630,7 @@ export interface EncryptFileRequest extends DecryptRequestBase {
   file: AcceptedFileType;
 }
 
-export interface DecryptRequest extends EncryptResponse, DecryptRequestBase {}
+export interface DecryptRequest extends EncryptResponse, DecryptRequestBase { }
 
 export interface DecryptResponse {
   // The decrypted data as a Uint8Array
@@ -635,10 +652,10 @@ export interface SigResponse {
 
 export interface ExecuteJsResponseBase {
   signatures:
-    | {
-        sig: SigResponse;
-      }
-    | any;
+  | {
+    sig: SigResponse;
+  }
+  | any;
 }
 
 /**
@@ -668,7 +685,7 @@ export interface ExecuteJsNoSigningResponse extends ExecuteJsResponseBase {
   logs: string;
 }
 
-export interface LitNodePromise {}
+export interface LitNodePromise { }
 
 export interface SendNodeCommand {
   url: string;
@@ -677,17 +694,17 @@ export interface SendNodeCommand {
 }
 export interface SigShare {
   sigType:
-    | 'BLS'
-    | 'K256'
-    | 'ECDSA_CAIT_SITH' // Legacy alias of K256
-    | 'EcdsaCaitSithP256';
+  | 'BLS'
+  | 'K256'
+  | 'ECDSA_CAIT_SITH' // Legacy alias of K256
+  | 'EcdsaCaitSithP256';
 
   signatureShare: string;
   shareIndex?: number;
   bigr?: string; // backward compatibility
   bigR?: string;
   publicKey: string;
-  dataSigned?: string;
+  dataSigned?: string | 'fail';
   siweMessage?: string;
   sigName?: string;
 }
@@ -704,6 +721,8 @@ export interface PkpSignedData {
 export interface NodeShare {
   claimData: any;
   shareIndex: any;
+
+  // I think this is deprecated
   unsignedJwt: any;
   signedData: SigShare;
   decryptedData: any;
@@ -1146,7 +1165,7 @@ export interface CommonGetSessionSigsProps {
 
 export interface BaseProviderGetSessionSigsProps
   extends CommonGetSessionSigsProps,
-    LitActionSdkParams {
+  LitActionSdkParams {
   /**
    * This is a callback that will be used to generate an AuthSig within the session signatures. It's inclusion is required, as it defines the specific resources and abilities that will be allowed for the current session.
    */
@@ -1155,7 +1174,7 @@ export interface BaseProviderGetSessionSigsProps
 
 export interface GetSessionSigsProps
   extends CommonGetSessionSigsProps,
-    LitActionSdkParams {
+  LitActionSdkParams {
   /**
    * This is a callback that will be used to generate an AuthSig within the session signatures. It's inclusion is required, as it defines the specific resources and abilities that will be allowed for the current session.
    */
@@ -1679,7 +1698,7 @@ export interface LoginUrlParams {
   error: string | null;
 }
 
-export interface BaseAuthenticateOptions {}
+export interface BaseAuthenticateOptions { }
 
 export interface EthWalletAuthenticateOptions extends BaseAuthenticateOptions {
   /**
@@ -1745,9 +1764,9 @@ export interface MintCapacityCreditsPerKilosecond
 }
 export interface MintCapacityCreditsContext
   extends MintCapacityCreditsPerDay,
-    MintCapacityCreditsPerSecond,
-    MintCapacityCreditsPerKilosecond,
-    GasLimitParam {}
+  MintCapacityCreditsPerSecond,
+  MintCapacityCreditsPerKilosecond,
+  GasLimitParam { }
 export interface MintCapacityCreditsRes {
   rliTxHash: string;
   capacityTokenId: any;
@@ -1870,12 +1889,12 @@ export interface LitActionSdkParams {
    * An object that contains params to expose to the Lit Action.  These will be injected to the JS runtime before your code runs, so you can use any of these as normal variables in your Lit Action.
    */
   jsParams?:
-    | {
-        [key: string]: any;
-        publicKey?: string;
-        sigName?: string;
-      }
-    | any;
+  | {
+    [key: string]: any;
+    publicKey?: string;
+    sigName?: string;
+  }
+  | any;
 }
 
 export interface LitEndpoint {
@@ -1897,7 +1916,7 @@ export interface SignerLike {
 
 export interface GetPkpSessionSigs
   extends CommonGetSessionSigsProps,
-    LitActionSdkParams {
+  LitActionSdkParams {
   pkpPublicKey: string;
 
   /**
@@ -1923,11 +1942,11 @@ export type GetLitActionSessionSigs = CommonGetSessionSigsProps &
   Pick<Required<LitActionSdkParams>, 'jsParams'> &
   (
     | (Pick<Required<LitActionSdkParams>, 'litActionCode'> & {
-        litActionIpfsId?: never;
-      })
+      litActionIpfsId?: never;
+    })
     | (Pick<Required<LitActionSdkParams>, 'litActionIpfsId'> & {
-        litActionCode?: never;
-      })
+      litActionCode?: never;
+    })
   ) & {
     ipfsOptions?: IpfsOptions;
   };
