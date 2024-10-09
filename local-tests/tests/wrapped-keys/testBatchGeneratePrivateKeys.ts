90a43cd6c3d3814b52b3b1cd194b5db9155f99e6
Daryl Collins <daryl@litprotocol.com>
2024-10-09 03:53:37 +0100

    test(wrapped-keys): LIT-3920 - Add assertions for signatures to `batchGeneratePrivateKeys` test - It's worth noting that these pass (and the code works) even with the 'broken' implementation that didn't use runOnce - probably coincidentally the sdk returns the first node's result, and that node matches.

--- local-tests/tests/wrapped-keys/testBatchGeneratePrivateKeys.ts
+++ local-tests/tests/wrapped-keys/testBatchGeneratePrivateKeys.ts
@@ -2,8 +2,75 @@ import { log } from '@lit-protocol/misc';
 import { TinnyEnvironment } from 'local-tests/setup/tinny-environment';
 import { api } from '@lit-protocol/wrapped-keys';
 import { getPkpSessionSigs } from 'local-tests/setup/session-sigs/get-pkp-session-sigs';
+import nacl from 'tweetnacl';
+import bs58 from 'bs58';
+import { ethers } from 'ethers';
+import { BatchGeneratePrivateKeysActionResult } from '../../../packages/wrapped-keys/src/lib/types';
 
-const { batchGeneratePrivateKeys } = api;
+const { batchGeneratePrivateKeys, exportPrivateKey } = api;
+
+async function verifySolanaSignature(
+  solanaResult: BatchGeneratePrivateKeysActionResult,
+  solanaMessageToSign
+) {
+  const {
+    signedMessage: { signature },
+    generatedPrivateKey: { generatedPublicKey },
+  } = solanaResult;
+  const signatureIsValidForPublicKey = nacl.sign.detached.verify(
+    Buffer.from(solanaMessageToSign),
+    bs58.decode(signature),
+    bs58.decode(generatedPublicKey)
+  );
+
+  console.log({ signatureIsValidForPublicKey, signature });
+  if (!signatureIsValidForPublicKey) {
+    throw new Error(
+      `signature: ${signature} doesn't validate for the Solana public key: ${generatedPublicKey}`
+    );
+  }
+}
+async function verifyEvmSignature(
+  litNodeClient,
+  evmResult,
+  messageToSign,
+  pkpSessionSigs
+) {
+  function verifyMessageSignature() {
+    try {
+      return ethers.utils.verifyMessage(
+        messageToSign,
+        evmResult.signedMessage.signature
+      );
+    } catch (err) {
+      throw new Error(
+        `When validating signed Ethereum message is valid: ${err.message}`
+      );
+    }
+  }
+
+  const { decryptedPrivateKey } = await exportPrivateKey({
+    litNodeClient,
+    network: 'evm',
+    id: evmResult.generatedPrivateKey.id,
+    pkpSessionSigs,
+  });
+
+  const recoveredAddress = verifyMessageSignature();
+
+  const wallet = new ethers.Wallet(decryptedPrivateKey);
+
+  console.log({
+    recoveredAddress,
+    walletAddress: wallet.address,
+    signature: evmResult.signedMessage.signature,
+  });
+  if (recoveredAddress !== wallet.address) {
+    throw new Error(
+      "Recovered address from verifyMessage doesn't match the wallet address"
+    );
+  }
+}
 
 /**
  * Test Commands:
@@ -24,17 +91,19 @@ export const testBatchGeneratePrivateKeys = async (
       new Date(Date.now() + 1000 * 60 * 10).toISOString()
     ); // 10 mins expiry
 
+    const solanaMessageToSign = 'This is a test solana message';
+    const evmMessageToSign = 'This is a test evm message';
     const results = await batchGeneratePrivateKeys({
       pkpSessionSigs: pkpSessionSigsSigning,
       actions: [
         {
           network: 'evm',
-          signMessageParams: { messageToSign: 'This is a test evm message' },
+          signMessageParams: { messageToSign: evmMessageToSign },
           generateKeyParams: { memo: 'Test evm key' },
         },
         {
           network: 'solana',
-          signMessageParams: { messageToSign: 'This is a test solana message' },
+          signMessageParams: { messageToSign: solanaMessageToSign },
           generateKeyParams: { memo: 'Test solana key' },
         },
       ],
@@ -63,6 +132,16 @@ export const testBatchGeneratePrivateKeys = async (
       throw new Error('Missing message signature in response');
     }
 
+    console.log('solana verify sig');
+    await verifySolanaSignature(results[1], solanaMessageToSign);
+
+    console.log('evm verify sig');
+    await verifyEvmSignature(
+      devEnv.litNodeClient,
+      results[0],
+      evmMessageToSign,
+      pkpSessionSigsSigning
+    );
     console.log('results', results);
 
     log('✅ testBatchGenerateEncryptedKeys');
