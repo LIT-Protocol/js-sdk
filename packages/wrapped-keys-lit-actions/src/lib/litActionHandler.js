141f0a00e1909e5d8d8501580652ac86a9a065e6
Daryl Collins <daryl@litprotocol.com>
2024-10-12 02:24:55 +0100

    feat(wrapped-keys-lit-actions): LIT-3959 - Ensure string values are not escaped using JSON.stringify() in `litActionHandler()`

--- packages/wrapped-keys-lit-actions/src/lib/litActionHandler.js
+++ packages/wrapped-keys-lit-actions/src/lib/litActionHandler.js
@@ -5,7 +5,13 @@ import { AbortError } from './abortError';
 export async function litActionHandler(actionFunc) {
   try {
     const litActionResult = await actionFunc();
-    Lit.Actions.setResponse({ response: JSON.stringify(litActionResult) });
+    // Don't re-stringify a string; we don't want to double-escape it
+    const response =
+      typeof litActionResult === 'string'
+        ? litActionResult
+        : JSON.stringify(litActionResult);
+
+    Lit.Actions.setResponse({ response });
   } catch (err) {
     // AbortError means exit immediately and do _NOT_ set a response
     // Nested code should really only throw this in cases where using e.g. `decryptToSingleNode`
