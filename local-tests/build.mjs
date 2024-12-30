7cae5d849f0f522a2c0f0ac49f1b69f045cb3fbc
Anson <ansonox@gmail.com>
2024-12-30 22:34:39 +0000

    feat: add back the CLI version of tinny to fix CI

--- local-tests/build.mjs
+++ local-tests/build.mjs
@@ -5,10 +5,37 @@ import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
 /**
- * Builds the project using esbuild.
+ * Build the CLI enabled version of Tinny.
  * @returns {Promise<void>} A promise that resolves when the build is complete.
  */
 export const build = async () => {
+  await esbuild.build({
+    entryPoints: [fileURLToPath(new URL('./test.ts', import.meta.url))],
+    outfile: fileURLToPath(new URL('./build/test.mjs', import.meta.url)),
+    bundle: true,
+    plugins: [
+      nodeExternalsPlugin({
+        allowList: [
+          'ethers',
+          '@lit-protocol/accs-schemas',
+          '@lit-protocol/contracts',
+          'crypto',
+          'secp256k1',
+        ],
+      }),
+    ],
+    platform: 'node',
+    target: 'esnext',
+    format: 'esm',
+    inject: [fileURLToPath(new URL('./shim.mjs', import.meta.url))],
+    mainFields: ['module', 'main'],
+  });
+};
+
+/**
+ * Bundle Tinny to be a standalone package.
+ */
+export const bundle = async () => {
   await esbuild.build({
     entryPoints: [fileURLToPath(new URL('./index.ts', import.meta.url))],
     outfile: fileURLToPath(new URL('./index.js', import.meta.url)),
@@ -37,5 +64,6 @@ export const build = async () => {
 (async () => {
   const start = Date.now();
   await build();
+  await bundle();
   console.log(`[build.mjs] 🚀 Build time: ${Date.now() - start}ms`);
 })();
