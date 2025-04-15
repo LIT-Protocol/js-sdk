71d50a4046e1d2cfaef6aaa0aed408e740996cc7
Anson <ansonox@gmail.com>
2025-04-15 21:21:18 +0100

    fix: add Object.freeze

--- packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
+++ packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
@@ -1,22 +1,22 @@
 import { LitCidRepository, LitCidRepositoryCommon } from './types';
 
 const LIT_ACTION_CID_REPOSITORY: LitCidRepository = Object.freeze({
-  signTransaction: {
+  signTransaction: Object.freeze({
     evm: 'QmYTEJyniRRkTxe3D9mmkURXCFBAXWK7TEGk5LxvWSETWn',
     solana: 'QmWUozjthkrjEBGkkEvGTvCaxCaiPMTke6FUpiwhn28k8Z',
-  },
-  signMessage: {
+  }),
+  signMessage: Object.freeze({
     evm: 'Qmdm4mGn6A8RmqeiDgRPXFt2yYEKCUMUVimSb5iPJfs31e',
     solana: 'QmS4Y6f2zHriNzioxBbysbuXQbjX7ga468CCyYcuY2AeeH',
-  },
-  generateEncryptedKey: {
+  }),
+  generateEncryptedKey: Object.freeze({
     evm: 'QmfW6g5PJ8SVS56XwDVC5W4gcUnobEempNkR28bj2g99tk',
     solana: 'QmWYcBCZqFmJJzsRfzVRHbPD5Hvuou45sbXPQcQzLbUgKd',
-  },
-  exportPrivateKey: {
+  }),
+  exportPrivateKey: Object.freeze({
     evm: 'QmfZ8hYEmNFwHraNieSz25dAp65YBoNjAMC2tXm9oaQCt6',
     solana: 'QmfZ8hYEmNFwHraNieSz25dAp65YBoNjAMC2tXm9oaQCt6',
-  },
+  }),
 });
 
 const LIT_ACTION_CID_REPOSITORY_COMMON: LitCidRepositoryCommon = Object.freeze({
