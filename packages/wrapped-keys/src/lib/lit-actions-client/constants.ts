5ab601b37c02983432d021fa7fcd61d8b8bb9dd6
Anson <ansonox@gmail.com>
2025-04-15 21:06:28 +0100

    fix(wrapped-keys): update ipfs cids

--- packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
+++ packages/wrapped-keys/src/lib/lit-actions-client/constants.ts
@@ -1,22 +1,22 @@
 import { LitCidRepository, LitCidRepositoryCommon } from './types';
 
 const LIT_ACTION_CID_REPOSITORY: LitCidRepository = Object.freeze({
-  signTransaction: Object.freeze({
-    evm: 'QmRpAgGKEmgeBRhqdC2EH17QUt6puwsbm8Z2nNneVN4uJG',
-    solana: 'QmR1nPG2tnmC72zuCEMZUZrrMEkbDiMPNHW45Dsm2n7xnk',
-  }),
-  signMessage: Object.freeze({
-    evm: 'QmXi9iqJvXrHoUGSo5WREonrruDhzQ7cFr7Cry3wX2hmue',
-    solana: 'QmcEJGVqRYtVukjm2prCPT7Fs66GpaqZwmZoxEHMHor6Jz',
-  }),
-  generateEncryptedKey: Object.freeze({
-    evm: 'QmeD6NYCWhUCLgxgpkgSguaKjwnpCnJ6Yf8SdsyPpK4eKK',
-    solana: 'QmPkVD3hEjMi1T5zQuvdrFCXaGTEMHNdAhAL4WHkqxijrQ',
-  }),
-  exportPrivateKey: Object.freeze({
-    evm: 'QmUJ74pTUqeeHzDGdfwCph1vJVNJ1rRzJdvMiTjS1BMwYj',
-    solana: 'QmUJ74pTUqeeHzDGdfwCph1vJVNJ1rRzJdvMiTjS1BMwYj',
-  }),
+  signTransaction: {
+    evm: 'QmYTEJyniRRkTxe3D9mmkURXCFBAXWK7TEGk5LxvWSETWn',
+    solana: 'QmWcAME1zaQ6jfE8yHCTxRhdonNmhvxhK71YvMKPWDXyBk',
+  },
+  signMessage: {
+    evm: 'Qmdm4mGn6A8RmqeiDgRPXFt2yYEKCUMUVimSb5iPJfs31e',
+    solana: 'QmS4Y6f2zHriNzioxBbysbuXQbjX7ga468CCyYcuY2AeeH',
+  },
+  generateEncryptedKey: {
+    evm: 'QmfW6g5PJ8SVS56XwDVC5W4gcUnobEempNkR28bj2g99tk',
+    solana: 'QmWYcBCZqFmJJzsRfzVRHbPD5Hvuou45sbXPQcQzLbUgKd',
+  },
+  exportPrivateKey: {
+    evm: 'QmfZ8hYEmNFwHraNieSz25dAp65YBoNjAMC2tXm9oaQCt6',
+    solana: 'QmfZ8hYEmNFwHraNieSz25dAp65YBoNjAMC2tXm9oaQCt6',
+  },
 });
 
 const LIT_ACTION_CID_REPOSITORY_COMMON: LitCidRepositoryCommon = Object.freeze({
