e5d3b31f7cf04ed0d86329259b728397c37d9285
Anson <ansonox@gmail.com>
2025-05-16 21:21:29 +0100

    remove: contracts-sdk

--- packages/contracts-sdk/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { getPriceFeedInfo, getNodePrices } from './lib/price-feed-info-manager';
-
-export * from './lib/contracts-sdk';
-export * from './lib/utils';
-
-export { getPriceFeedInfo, getNodePrices };
