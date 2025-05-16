9956c204e3408bec08d333d09371c2a9d5671562
Anson <ansonox@gmail.com>
2025-05-16 21:18:22 +0100

    remove: event-listener (we can add this back if someone requests it)

--- packages/event-listener/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/actions';
-export * from './lib/listeners';
-export * from './lib/states';
-export * from './lib/state-machine';
-export * from './lib/transitions';
-export * from './lib/types';
--- packages/event-listener/src/lib/listeners/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './constant';
-export * from './evm-block';
-export * from './evm-contract-event';
-export * from './fetch';
-export * from './interval';
-export * from './listener';
-export * from './timer';
