f3fdc4c7e3818289e2c524cd2cde156c1252270a
anson <ansonox@gmail.com>
2025-08-26 13:56:41 +0100

    refactor(networks): change request id prefix

--- packages/networks/src/networks/vNaga/shared/helpers/createRequestId.ts
+++ packages/networks/src/networks/vNaga/shared/helpers/createRequestId.ts
@@ -2,5 +2,5 @@
  * Creates a unique request ID for tracking requests
  */
 export function createRequestId(): string {
-  return `request_${Date.now()}_${Math.random().toString(36).substring(2, 15)}`;
+  return `lit_${Date.now()}_${Math.random().toString(36).substring(2, 15)}`;
 }
