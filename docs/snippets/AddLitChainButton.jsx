f4751e164c47e0415e5591b04e272f9ba5dd732c
Chris Cassano <chris@litprotocol.com>
2025-10-24 15:48:40 -0700

    add lit chain button and more chain info

---
+++ docs/snippets/AddLitChainButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function AddLitChainButton() {
+  const addNetwork = () => {
+    const params = [
+      {
+        chainId: '0x2AC60',
+        chainName: 'Lit Chain',
+        nativeCurrency: {
+          name: 'Lit Protocol',
+          symbol: 'LITKEY',
+          decimals: 18,
+        },
+        rpcUrls: ['https://lit-chain-rpc.litprotocol.com'],
+        blockExplorerUrls: [
+          'https://lit-chain-explorer.litprotocol.com',
+        ],
+      },
+    ];
+
+    window.ethereum
+      .request({ method: 'wallet_addEthereumChain', params })
+      .then(() => console.log('Success'))
+      .catch(error => console.log('Error', error.message));
+  };
+
+  return (
+    <button onClick={addNetwork}>Add Lit Chain to Metamask</button>
+  );
+}
