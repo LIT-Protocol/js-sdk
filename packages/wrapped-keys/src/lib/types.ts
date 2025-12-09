9e842578181a0dc876f6f75b283459074d40c370
anson <ansonox@gmail.com>
2025-12-09 21:41:56 +0000

    fix: https://github.com/LIT-Protocol/js-sdk/pull/1006#discussion_r2604446453

--- packages/wrapped-keys/src/lib/types.ts
+++ packages/wrapped-keys/src/lib/types.ts
@@ -80,6 +80,20 @@ export interface StoredKeyData extends StoredKeyMetadata {
   dataToEncryptHash: string;
 }
 
+/** Represents a historical version of a wrapped key after an update operation
+ * Contains the previous state of the key including ciphertext and metadata
+ *
+ * @typedef WrappedKeyVersion
+ * @property { string } id The unique identifier (UUID V4) of this version
+ * @property { string } ciphertext The base64 encoded, salted & encrypted private key for this version
+ * @property { string } dataToEncryptHash SHA-256 hash of the ciphertext for this version
+ * @property { KeyType } keyType The type of key (e.g. ed25519, K256)
+ * @property { LIT_NETWORKS_KEYS } litNetwork The LIT network that the client was connected to
+ * @property { string } memo The user-provided descriptor for this version
+ * @property { string } publicKey The public key of the encrypted private key
+ * @property { string } [evmContractConditions] Optional EVM contract conditions
+ * @property { string } updatedAt ISO 8601 timestamp of when this version was created
+ */
 export interface WrappedKeyVersion
   extends Pick<
     StoredKeyData,
